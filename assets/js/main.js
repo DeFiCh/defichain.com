9342576dda16f73092cc94286c16d70d09f5c6ad
Aen <hello@aentan.com>
2020-01-08 14:18:45 +0800

    fix scrollspy offset

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,7 +72,10 @@ $(function () {
     });
   }
 
-  $('body').scrollspy({ target: '#TableOfContents' });
+  $('body').scrollspy({
+    target: '#TableOfContents',
+    offset: 154
+  });
 
   $("#TableOfContents").stick_in_parent({
     offset_top: 144

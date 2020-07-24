1afe02060d2d964b2c47cf75953d84ede385a419
Saurabh Shinde <saurabhshinde391@gmail.com>
2020-07-24 22:53:08 +0530

    Added Recaptcha in frontend form

--- assets/js/claim.js
+++ assets/js/claim.js
@@ -1,5 +1,11 @@
+var onloadCallback = function () {
+  grecaptcha.render('google_recaptcha', {
+    'sitekey': '6LcSh7UZAAAAAK2s75MiBrNlmzq1BGyI4PI4KD60'
+  });
+};
+
 $(document).ready(function () {
-  var base_url = "https://defichain.com/";
+  var base_url = "https://defichain.com";
 
   // Handle wizard next buttons
   function wizardNextStep() {
@@ -90,32 +96,39 @@ $(document).ready(function () {
     if (!data.email) {
       delete data['email'];
     }
-    $.ajax({
-      data: JSON.stringify(data),
-      url: base_url + "/claim",
-      dataType: "json",
-      type: 'POST',
-      beforeSend: function () {
-        $('#claim-step4 .claim-wizard-next + .spinner').addClass('show');
-      },
-      success: function (response) {
-        $('#step4-error-alert').hide();
-        $('#claim-step4').css({
-          'height': 'auto'
-        });
-        var rTF = moment(parseInt(response.transactionInfo.requestedTs)).format("ddd, MMM D YYYY, h:mm a");
-        $('#claim-step4 .claim-wizard-next + .spinner').removeClass('show');
-        $('#dfi-status-value').html(response.transactionInfo.status);
-        $('#requested-ts-value').html(rTF);
-        $('#step4-success-alert').show();
-        $('body').scrollTo('+=272', 500);
-      },
-      error: function (response) {
-        $('#claim-step4 .claim-wizard-next + .spinner').removeClass('show');
-        $('.alert').html(response.responseJSON.error.message);
-        $('#step4-error-alert').show();
-      }
-    });
+    var rcres = grecaptcha.getResponse();
+    if (!rcres.length) {
+      $('#claim-step4 .claim-wizard-next + .spinner').removeClass('show');
+      $('.alert').html("Please select recaptcha before proceed");
+      $('#step4-error-alert').show();
+    } else {
+      $.ajax({
+        data: JSON.stringify(data),
+        url: base_url + "/claim",
+        dataType: "json",
+        type: 'POST',
+        beforeSend: function () {
+          $('#claim-step4 .claim-wizard-next + .spinner').addClass('show');
+        },
+        success: function (response) {
+          $('#step4-error-alert').hide();
+          $('#claim-step4').css({
+            'height': 'auto'
+          });
+          var rTF = moment(parseInt(response.transactionInfo.requestedTs)).format("ddd, MMM D YYYY, h:mm a");
+          $('#claim-step4 .claim-wizard-next + .spinner').removeClass('show');
+          $('#dfi-status-value').html(response.transactionInfo.status);
+          $('#requested-ts-value').html(rTF);
+          $('#step4-success-alert').show();
+          $('body').scrollTo('+=272', 500);
+        },
+        error: function (response) {
+          $('#claim-step4 .claim-wizard-next + .spinner').removeClass('show');
+          $('.alert').html(response.responseJSON.error.message);
+          $('#step4-error-alert').show();
+        }
+      });
+    }
   });
 
 })

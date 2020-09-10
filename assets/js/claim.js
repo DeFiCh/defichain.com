af5602d369bf37f556574865cdea750888fd5b92
Aen <hello@aentan.com>
2020-09-10 15:18:18 +0800

    fix claim endpoint URL

--- assets/js/claim.js
+++ assets/js/claim.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
     refreshBalance();
   }, 30 * 1000);
 
-  var base_url = "https://airdrop-api.defichain.com/";
+  var base_url = "https://airdrop-api.defichain.com";
 
   // Handle wizard next buttons
   function wizardNextStep() {

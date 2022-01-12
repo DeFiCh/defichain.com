0bfbad2854bfb4d5caa9046e7bef41a33034c1bc
Joel-David Wong <git@joeldavid.dev>
2022-01-12 19:45:48 +0800

    update cypress

---
+++ cypress/integration/pages/index.spec.ts
@@ -0,0 +1,19 @@
+context('/ on macbook-16', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  beforeEach(() => {
+    cy.viewport('macbook-16')
+  })
+})
+
+context('/ on iphone-x', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  beforeEach(() => {
+    cy.viewport('iphone-x')
+  })
+})

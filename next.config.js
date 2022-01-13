f116207b62e00887dbf3950b4f6be623c70abeea
Joel-David Wong <git@joeldavid.dev>
2022-01-13 12:35:17 +0800

    Added security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,54 @@
 const { i18n } = require('./next-i18next.config');
 
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'none';` +
+      `base-uri 'none';` +
+      `child-src 'self' app.netlify.com;` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self' data:;` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' app.netlify.com netlify-cdp-loader.netlify.app ${process.env.NODE_ENV === 'development' ? `'unsafe-eval'` : ''};` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline';` +
+      `font-src fonts.gstatic.com;` +
+      `connect-src 'self' ocean.defichain.com;` +
+      `prefetch-src 'self';`
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  }
+]
+
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
-  i18n
+  i18n,
+  async headers () {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }

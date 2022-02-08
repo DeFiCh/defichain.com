1e269ae6aaa63638010f1d74848a4eefa7091fa7
Joel-David Wong <git@joeldavid.dev>
2022-02-08 10:25:59 +0800

    Removed CoinPedia and moved ResourceCard into a component

--- src/pages/developers/_components/DeveloperResourceSection.tsx
+++ src/pages/developers/_components/DeveloperResourceSection.tsx
@@ -1,9 +1,7 @@
-import { PropsWithChildren } from 'react'
-import { ExternalLink } from '@components/commons/link/ExternalLink'
-import NextLink from 'next/link'
 import { BsFillTerminalFill, BsGithub, BsReddit, BsSticky } from 'react-icons/bs'
 import { DeFiChainLogoNoText } from '@components/icons/DeFiChainLogoNoText'
 import { useTranslation } from 'next-i18next'
+import { ResourceCard } from '@components/commons/ResourceCard'
 
 export function DeveloperResourceSection (): JSX.Element {
   const { t } = useTranslation('developers')
@@ -54,48 +52,5 @@ export function DeveloperResourceSection (): JSX.Element {
         <BsReddit fontSize={50} className='fill-primary-500' />
       </ResourceCard>
     </section>
-  )
-}
-
-export function ResourceCard (props: PropsWithChildren<{ external?: boolean, text: string, buttonText: string, url: string, testid: string }>): JSX.Element {
-  return (
-    <div className='p-1.5 w-full lg:w-1/2'>
-      <div
-        className='rounded bg-gray-50 py-10 px-16 space-y-10'
-        data-testid={props.testid}
-      >
-        <div className='w-full'>
-          {props.children}
-        </div>
-
-        <div className='flex flex-col space-y-2'>
-          <span className='text-lg md:text-xl'>{props.text}</span>
-        </div>
-
-        <div>
-          {(() => {
-            if (props.external !== undefined) {
-              return (
-                <ExternalLink url={props.url}>
-                  <button
-                    className='button rounded py-2 px-6 font-medium text-white bg-primary-500 hover:bg-primary-900'
-                  >
-                    {props.buttonText}
-                  </button>
-                </ExternalLink>
-              )
-            }
-
-            return (
-              <NextLink href={props.url}>
-                <button className='button rounded py-2 px-6 font-medium text-white bg-primary-500 hover:bg-primary-900'>
-                  {props.buttonText}
-                </button>
-              </NextLink>
-            )
-          })()}
-        </div>
-      </div>
-    </div>
   )
 }

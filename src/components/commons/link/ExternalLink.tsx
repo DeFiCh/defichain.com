c7592591fbff882c621e729e10171ac1561a7dc6
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-15 15:46:25 +0100

    Added classname props to ExternalLink.tsx

--- src/components/commons/link/ExternalLink.tsx
+++ src/components/commons/link/ExternalLink.tsx
@@ -1,8 +1,7 @@
 import { PropsWithChildren } from 'react'
-
-export function ExternalLink (props: PropsWithChildren<{ url: string, testId?: string }>): JSX.Element {
+export function ExternalLink (props: PropsWithChildren<{ url: string, testId?: string, classname?: string }>): JSX.Element {
   return (
-    <a href={props.url} target='_blank' rel='noreferrer' data-testid={props.testId}>
+    <a href={props.url} className={props.classname} target='_blank' rel='noreferrer' data-testid={props.testId}>
       {props.children}
     </a>
   )

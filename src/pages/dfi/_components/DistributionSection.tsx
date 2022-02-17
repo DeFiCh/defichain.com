e426775ed6370a438e73ec7b4bb8ef1cc6547a3f
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-17 15:01:00 +0100

    Clean up Distribution Section

--- src/pages/dfi/_components/DistributionSection.tsx
+++ src/pages/dfi/_components/DistributionSection.tsx
@@ -5,16 +5,25 @@ export function DistributionSection (): JSX.Element {
   return (
     <section className='py-20'>
       <Container>
-        <div className='flex flex-col w-full  md:w-1/3 lg:w-1/2 mb-16 space-y-10'>
+        <div className='flex flex-col w-full md:w-1/3 lg:w-1/2 mb-16 space-y-10'>
           <h1 className='font-medium text-2xl md:text-5xl' data-testid='DistributionSection.Heading'>Initial Token Distribution</h1>
-          <div className='text-xl md:text-2xl' data-testid='DistributionSection.Text'> Of the 1.2 billion $DFI coins, 49% will be in the initial supply, with the rest issued to masternode holders over time.</div>
+          <div className='text-xl md:text-2xl' data-testid='DistributionSection.Text'>
+            Of the 1.2 billion $DFI coins, 49% will be in the initial supply, with the rest issued to masternode holders over time.
+          </div>
         </div>
-        <div className='mx-auto w-full md:w-[60%] space-y-16' data-testid='DistributionSection.Chart'>
-          <Chart labelA='Initial supply' labelB='To masternodes over time' />
+        <div className='mx-auto w-full md:w-3/5 space-y-16' data-testid='DistributionSection.Chart'>
+          <Chart
+            labelA='Initial supply'
+            labelB='To masternodes over time'
+          />
           <div>
             Of the initial supply, 49% will be kept by the DeFiChain Foundation. The rest may be distributed to external partners, to fund the initial development.
           </div>
-          <Chart labelA='DeFiChain Foundation' labelB='External partners' fill />
+          <Chart
+            labelA='DeFiChain Foundation'
+            labelB='External partners'
+            fill
+          />
         </div>
       </Container>
     </section>

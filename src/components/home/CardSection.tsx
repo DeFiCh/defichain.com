c05037449f5648e39f63e4733e490dfbc2e28fd4
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-17 13:33:40 +0100

    slick slider on sections

--- src/components/home/CardSection.tsx
+++ src/components/home/CardSection.tsx
@@ -1,16 +1,52 @@
-import { Container } from '@components/commons/Container'
 import classNames from 'classnames'
+import Slider from 'react-slick'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 
 export function CardSection (): JSX.Element {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrow: false,
+    lazyload: 'ondemand',
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    responsive: [
+      {
+        breakpoint: 1180,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
   return (
-    <section className='-mt-56'>
-      <Container>
-        <div className='flex flex-col space-y-4 lg:space-y-0 items-center lg:items-start lg:flex-row item-center'>
-          <Card title='Latest feature' subtitle='Decentralized Assets' desc='Access decentralized assets with collateral in your vault.' testId='CardSection.LatestFeature' url='/' buttonText='Learn more' />
-          <Card title='featured download' subtitle='DefiChain Wallet' desc='Experience DeFiChain with wallet apps built for both desktop and mobile.' testId='CardSection.Downloads' url='/' buttonText='Go to downloads' />
-          <Card title='how-to guide' subtitle='Buy $DFI' desc='Learn about and acquire the $DFI coin from various crypto exchanges.' testId='CardSection.HowTo' url='/' buttonText='Learn more' fill />
-        </div>
-      </Container>
+    <section className='-mt-80'>
+      <Slider {...settings}>
+        <Card title='Latest feature' subtitle='Decentralized Assets' desc='Access decentralized assets with collateral in your vault.' testId='CardSection.LatestFeature' url='/' buttonText='Learn more' />
+        <Card title='featured download' subtitle='DefiChain Wallet' desc='Experience DeFiChain with wallet apps built for both desktop and mobile.' testId='CardSection.Downloads' url='/' buttonText='Go to downloads' />
+        <Card title='how-to guide' subtitle='Buy $DFI' desc='Learn about and acquire the $DFI coin from various crypto exchanges.' testId='CardSection.HowTo' url='/' buttonText='Learn more' fill />
+        <Card title='how-to guide' subtitle='Buy $DFI' desc='Learn about and acquire the $DFI coin from various crypto exchanges.' testId='CardSection.HowTo' url='/' buttonText='Learn more' fill />
+        <Card title='how-to guide' subtitle='Buy $DFI' desc='Learn about and acquire the $DFI coin from various crypto exchanges.' testId='CardSection.HowTo' url='/' buttonText='Learn more' fill />
+      </Slider>
     </section>
   )
 }
@@ -37,5 +73,21 @@ function Card (props: Cardprops): JSX.Element {
       </div>
       <a className={classNames('inline-flex shadow rounded justify-center mt-auto max-w-fit py-3 px-5 rounded', props.fill !== undefined ? 'text-primary-500 border-2 border-primary-500' : 'bg-primary-500 text-white')} href={props.url}>{props.buttonText}</a>
     </div>
+  )
+}
+
+function PrevArrow (props: any): JSX.Element {
+  return (
+    <button className={classNames(props.className, '-top-[100px] left-[20px] xl:left-[900px] z-50')} style={props.style} onClick={props.onClick}>
+      <BsChevronLeft fontSize={50} fill='fill-black' />
+    </button>
+  )
+}
+
+function NextArrow (props: any): JSX.Element {
+  return (
+    <button className={classNames(props.className, '-top-[100px] right-[20px] xl:right-[200px] z-50')} style={props.style} onClick={props.onClick}>
+      <BsChevronRight fontSize={50} fill='fill-black' />
+    </button>
   )
 }
--- src/components/home/HomePageHeader.tsx
+++ src/components/home/HomePageHeader.tsx
@@ -10,8 +10,8 @@ export function HomePageHeader (): JSX.Element {
         backgroundPosition: 'right center'
       }}
     >
-      <Container className='h-full mt-36 mb-16 h-screen'>
-        <div className='mx-auto w-[70%] md:w-1/2'>
+      <Container className='h-full mt-36 mb-16 min-h-[560px] lg:min-h-[800px]'>
+        <div className='mx-auto w-full md:w-1/2'>
           <h1 className='text-4xl text-center lg:text-5xl text-gray-900 w-full mb-6 font-semibold' data-testid='Header.title'>
             Native Decentralized Finance for Bitcoin
           </h1>

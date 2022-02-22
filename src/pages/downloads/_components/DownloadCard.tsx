2c04d1202bb46b7d1bffb11d8beb071be29571f7
Suraj Auwal <siradjiawoual@gmail.com>
2022-02-22 11:52:14 +0100

    Update src/pages/downloads/_components/DownloadCard.tsx
    Co-authored-by: Joel-David Wong <4586209+joeldavidw@users.noreply.github.com>

--- src/pages/downloads/_components/DownloadCard.tsx
+++ src/pages/downloads/_components/DownloadCard.tsx
@@ -3,19 +3,19 @@ import NextImage from 'next/image'
 
 export function DownloadCard (props: PropsWithChildren<{ testid: string, title: string, desc: string, imageSrc: string | JSX.Element }>): JSX.Element {
   return (
-    <div className='p-1.5 w-full lg:w-1/2'>
+    <div className='p-1.5 w-full lg:w-1/2 flex flex-col'>
       <div
-        className='rounded bg-gray-50 py-10 px-16 space-y-10'
+        className='rounded bg-gray-50 py-10 px-16 space-y-10 flex flex-col flex-1'
         data-testid={props.testid}
       >
         {(typeof props.imageSrc === 'string')
-          ? (<div className='w-full'><NextImage src={props.imageSrc} width='64px' height='64px' /></div>)
+          ? (<div className='w-full'><NextImage src={props.imageSrc} width='64px' height='64px'/></div>)
           : props.imageSrc}
-        <div className='flex flex-col space-y-4'>
-          <h1 className='text-2xl font-semibold'>{props.title}</h1>
-          <div className='text-lg'>{props.desc}</div>
+        <h1 className='text-2xl font-semibold'>{props.title}</h1>
+        <div className='text-lg flex-1'>{props.desc}</div>
+        <div className='grow flex items-center'>
+          {props.children}
         </div>
-        {props.children}
       </div>
     </div>
   )

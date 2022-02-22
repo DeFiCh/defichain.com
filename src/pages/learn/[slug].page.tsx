dc3c363ac31a4c85532e3cb8776d4d2b5a78e1ed
Joel-David Wong <git@joeldavid.dev>
2022-02-22 17:50:57 +0800

    Added md layout

--- src/pages/learn/[slug].page.tsx
+++ src/pages/learn/[slug].page.tsx
@@ -1,83 +1,68 @@
 import fs from 'fs'
 import matter from 'gray-matter'
-import {MDXRemote} from 'next-mdx-remote'
-import {serialize} from 'next-mdx-remote/serialize'
-import Head from 'next/head'
-import Link from 'next/link'
+import { MDXRemote } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
 import path from 'path'
-import {getPostFilePaths, getPostPath} from './utils/mdxUtils'
-import {Container} from "@components/commons/Container";
+import { getPostFilePaths, getPostPath } from './utils/mdxUtils'
+import { Container } from '@components/commons/Container'
+import { Header } from '@components/commons/Header'
+import rehypeSlug from 'rehype-slug'
+import { GetStaticPathsResult, GetStaticPropsResult } from 'next'
+import { MDXRemoteSerializeResult } from 'next-mdx-remote/dist/types'
 
-// Custom components/renderers to pass to MDX.
-// Since the MDX files aren't loaded by webpack, they have no knowledge of how
-// to handle import statements. Instead, you must include components in scope
-// here.
-const components = {
-  Head,
+interface PostPageProps {
+  source: MDXRemoteSerializeResult
+  frontMatter: { [p: string]: any }
 }
 
-export default function PostPage({source, frontMatter}) {
+export default function PostPage ({ source, frontMatter }): JSX.Element {
   return (
-    <Container>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>👈 Go back home</a>
-          </Link>
-        </nav>
-      </header>
-      <div className="post-header">
-        <h1>{frontMatter.title}</h1>
-        {frontMatter.description && (
-          <p className="description">{frontMatter.description}</p>
-        )}
-      </div>
-      <main>
-        <MDXRemote {...source} components={components}/>
-      </main>
-
-      <style jsx>{`
-        .post-header h1 {
-          margin-bottom: 0;
-        }
-
-        .post-header {
-          margin-bottom: 2rem;
-        }
-
-        .description {
-          opacity: 0.6;
-        }
-      `}</style>
-    </Container>
+    <>
+      <Header title={frontMatter.title}>
+        <div className='mt-10 flex flex-wrap'>
+          <div className='w-full text-2xl text-gray-900' data-testid='Header.desc.main'>
+            {frontMatter.description}
+          </div>
+        </div>
+      </Header>
+      <Container>
+        <article className='prose lg:prose-xl mx-auto py-20'>
+          <MDXRemote {...source} />
+        </article>
+      </Container>
+    </>
   )
 }
 
-export const getStaticProps = async ({params, locale}) => {
-  const postFilePath = path.join(getPostPath(locale), `${params.slug}.mdx`)
+export async function getStaticProps ({ params, locale }): Promise<GetStaticPropsResult<PostPageProps>> {
+  const slug: string = params.slug
+  const postFilePath = path.join(getPostPath(locale), `${slug}.mdx`)
   const source = fs.readFileSync(postFilePath)
 
-  const {content, data} = matter(source)
+  const { content, data } = matter(source)
 
   const mdxSource = await serialize(content, {
-    scope: data,
+    mdxOptions: {
+      rehypePlugins: [rehypeSlug]
+    },
+    scope: data
   })
 
   return {
     props: {
       source: mdxSource,
-      frontMatter: data,
-    },
+      frontMatter: data
+    }
   }
 }
 
-export const getStaticPaths = async ({locales}) => {
-  const paths = locales.map(locale => getPostFilePaths(locale)
+export async function getStaticPaths (context): Promise<GetStaticPathsResult> {
+  const paths = context.locales?.map(locale => getPostFilePaths(locale)
     .map((path) => path.replace(/\.mdx?$/, ''))
-    .map((slug) => ({params: {slug}, locale: locale}))).flat()
+    .map((slug) => ({ params: { slug }, locale: locale }))).flat()
 
   return {
     paths,
-    fallback: false,
+    fallback: false
   }
 }

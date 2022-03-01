aa44aadaa6718fa71b081917b2c4a109aa04184d
Suraj Auwal <siradjiawoual@gmail.com>
2022-03-01 13:45:41 +0100

    Home header section redo

--- src/components/home/HomePageHeader.tsx
+++ src/components/home/HomePageHeader.tsx
@@ -10,14 +10,14 @@ export function HomePageHeader (): JSX.Element {
         backgroundPosition: 'right center'
       }}
     >
-      <Container className='h-full mt-36 mb-16 min-h-[560px] lg:min-h-[800px]'>
+      <Container className='h-full mt-36 mb-16 min-h-[35rem] lg:min-h-[40rem]'>
         <div className='mx-auto w-full md:w-1/2'>
           <h1 className='text-4xl text-center lg:text-5xl text-gray-900 w-full mb-6 font-semibold' data-testid='Header.title'>
             Native Decentralized Finance for Bitcoin
           </h1>
           <div className='h-full flex flex-wrap items-center'>
             <div className='mt-10 flex flex-wrap'>
-              <div className='w-full text-2xl text-gray-900 text-center' data-testid='Header.desc.main'>A blockchain dedicated to fast, intelligent and transparent decentralized financial services, accessible by everyone.
+              <div className='w-full text-lg md:text-2xl text-gray-900 text-center' data-testid='Header.desc.main'>A blockchain dedicated to fast, intelligent and transparent decentralized financial services, accessible by everyone.
               </div>
               <br />
             </div>

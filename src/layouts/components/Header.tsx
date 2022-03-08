b505f7051ab6e11eeda13141318d4a9a28da5eeb
Joel-David Wong <git@joeldavid.dev>
2022-03-08 19:44:22 +0800

    Added exchanges

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -7,7 +7,9 @@ import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { useWhaleApiClient } from '../context/WhaleContext'
 import { useTranslation } from 'next-i18next'
-import { Listbox, Transition } from '@headlessui/react'
+import { Listbox, Popover, Transition } from '@headlessui/react'
+import { ExternalLink } from '@components/commons/link/ExternalLink'
+import Image from 'next/image'
 
 export function Header (): JSX.Element {
   const [menu, setMenu] = useState(false)
@@ -153,7 +155,7 @@ function MobileMenu ({ price }: { price: string }): JSX.Element {
             className='flex justify-center border-b border-gray-100' text={t('header.navbar.learn')} pathname='/learn'
             testId='Mobile.HeaderLink.Learn'
           />
-          <ExternalHeaderLink
+          <ExternalLink
             className='p-2 flex justify-center border-b border-gray-100' text='DeFi Scan' url='https://defiscan.live/'
             testId='Mobile.HeaderLink.DeFiScan'
           />
@@ -200,18 +202,77 @@ export function ExternalHeaderLink (props: { text: string, url: string, classNam
 function BuyDfiButton ({ classname, price }: { classname?: string, price: string }): JSX.Element {
   const { t } = useTranslation('layout')
 
+  const exchanges = [
+    { name: 'DFX', image: '/assets/svg/exchanges/logo-dfx.svg', url: 'https://dfx.swiss/en/' },
+    { name: 'KuCoin', image: '/assets/svg/exchanges/logo-kucoin.svg', url: 'https://trade.kucoin.com/DFI-BTC' },
+    {
+      name: 'Bittrex',
+      image: '/assets/svg/exchanges/logo-bittrex.svg',
+      url: 'https://global.bittrex.com/Market/Index?MarketName=BTC-DFI'
+    },
+    { name: 'LATOKEN', image: '/assets/svg/exchanges/logo-latoken.svg', url: 'https://go.latoken.com/1gd' },
+    {
+      name: 'Hotbit',
+      image: '/assets/img/exchanges/logo-hotbit.png',
+      url: 'https://www.hotbit.io/exchange?symbol=DFI_USDT'
+    },
+    { name: 'Bittrue', image: '/assets/svg/exchanges/logo-bittrue.svg', url: 'https://www.bitrue.com/trade/dfi_btc' },
+    {
+      name: 'EasyCrypto (AU)',
+      image: '/assets/img/exchanges/logo-easycrypto-au.png',
+      url: 'https://easycrypto.ai/au/buy-sell/dfi-defichain'
+    },
+    {
+      name: 'EasyCrypto (NZ)',
+      image: '/assets/img/exchanges/logo-easycrypto-nz.png',
+      url: 'https://easycrypto.ai/nz/buy-sell/dfi-defichain'
+    },
+    {
+      name: 'Transak',
+      image: '/assets/img/exchanges/logo-transak.png',
+      url: 'https://global.transak.com/?apiKey=9cb22c33-f1fc-4ff0-9736-9159bb879568&cryptoCurrencyCode=DFI'
+    },
+    { name: 'Hoo', image: '/assets/img/exchanges/logo-hoo.png', url: 'https://hoo.com/innovation/dfi-usdt' }
+  ]
+
   return (
-    <div
-      className={classNames('flex justify-center items-center text-white bg-primary-500 p-2 xl:px-4 lg:rounded md:mb-0', classname)}
-    >
-      {t('header.navbar.buy')}
-      <span className='font-medium'>$DFI</span>
-      {
-        price !== '0' && (
-          <span className='ml-1.5 text-gray-100 font-medium text-sm'>${Number(price).toFixed(2)}</span>
-        )
-      }
-    </div>
+    <Popover className='relative'>
+      <Popover.Button>
+        <div
+          className={classNames('flex justify-center items-center text-white bg-primary-500 p-2 xl:px-4 lg:rounded md:mb-0', classname)}
+        >
+          {t('header.navbar.buy')}
+          <span className='font-medium'>$DFI</span>
+          {
+            price !== '0' && (
+              <span className='ml-1.5 text-gray-100 font-medium text-sm'>${Number(price).toFixed(2)}</span>
+            )
+          }
+        </div>
+      </Popover.Button>
+
+      <Popover.Panel
+        className='mt-2 bg-white w-32 rounded absolute z-50 text-center text-gray-700 text-lg border shadow-lg border-gray-200'
+      >
+        {
+          exchanges.map(exchange => (
+            <div key={exchange.name}>
+              <ExternalLink url={exchange.url}>
+                <div className='flex justify-center p-3 hover:bg-gray-100 border-b'>
+                  <Image
+                    src={exchange.image ?? ''}
+                    width={100}
+                    height={30}
+                    layout='fixed'
+                    objectFit='contain'
+                  />
+                </div>
+              </ExternalLink>
+            </div>
+          ))
+        }
+      </Popover.Panel>
+    </Popover>
   )
 }
 
@@ -241,7 +302,7 @@ function LanguageDropdown (): JSX.Element {
           leaveTo='transform scale-95 opacity-0'
         >
           <div
-            className='bg-white w-32 rounded absolute z-50 text-center text-gray-700 text-lg border shadow-lg border-gray-200'
+            className='mt-2 bg-white w-32 rounded absolute z-50 text-center text-gray-700 text-lg border shadow-lg border-gray-200'
           >
             <Listbox.Options>
               {languages.map((language) => (

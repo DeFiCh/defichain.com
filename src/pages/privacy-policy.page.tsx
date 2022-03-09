ef34daee098d791749c73ebf01156907d6e9c090
Joel-David Wong <git@joeldavid.dev>
2022-03-09 19:30:35 +0800

    Added privacy policy page

---
+++ src/pages/privacy-policy.page.tsx
@@ -0,0 +1,71 @@
+import { UserConfig } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { Header } from '@components/commons/Header'
+import { Container } from '@components/commons/Container'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import React from 'react'
+import { Post } from './learn/utils/api'
+import { remark } from 'remark'
+import { getMDPageBySlug } from '../../utils/api'
+import { Head } from '@components/commons/Head'
+
+interface PrivacyPolicyPageProps {
+  props: {
+    _nextI18Next: { initialI18nStore: any, initialLocale: string, userConfig: UserConfig | null }
+    post: Post
+  }
+}
+
+export default function PrivacyPolicyPage ({ post }): JSX.Element {
+  return (
+    <>
+      <Head
+        title={post.title}
+        description={post.description}
+      />
+      <Header title={post.title}>
+        <div className='mt-10 flex flex-wrap'>
+          <div className='w-full text-2xl text-gray-900' data-testid='Header.desc.main'>
+            {post.description}
+          </div>
+        </div>
+      </Header>
+      <Container>
+        <article className='prose lg:prose-xl mx-auto py-20'>
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeSanitize, rehypeSlug, [rehypeAutolinkHeadings, {
+              behavior: 'wrap',
+              properties: {
+                className: ['no-underline hover:underline']
+              }
+            }]]}
+          >
+            {post.content}
+          </ReactMarkdown>
+        </article>
+      </Container>
+    </>
+  )
+}
+
+export async function getStaticProps ({ locale }): Promise<PrivacyPolicyPageProps> {
+  const post = getMDPageBySlug('privacy-policy', locale)
+
+  const result = await remark()
+    .process(post.content)
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'layout'])),
+      post: {
+        ...post,
+        content: result.toString()
+      }
+    }
+  }
+}

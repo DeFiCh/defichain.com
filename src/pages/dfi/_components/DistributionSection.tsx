a5e8b3faaaf96e16ee78dafd81e78e0218aa8ec9
Joel-David Wong <git@joeldavid.dev>
2022-03-24 16:57:09 +0800

    Updated chart

--- src/pages/dfi/_components/DistributionSection.tsx
+++ src/pages/dfi/_components/DistributionSection.tsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames'
 import { Container } from '@components/commons/Container'
-import { useTranslation } from 'next-i18next'
+import { Trans, useTranslation } from 'next-i18next'
 import { Section } from '@components/commons/Section'
+import { ExternalLink } from '@components/commons/link/ExternalLink'
+import React from 'react'
 
 export function DistributionSection (): JSX.Element {
   const { t } = useTranslation(['page-dfi'])
@@ -9,22 +11,28 @@ export function DistributionSection (): JSX.Element {
   return (
     <Container>
       <Section title={t('DistributionSection.title')} testId='DistributionSection'>
-        <div className='flex flex-col w-full md:w-1/3 lg:w-1/2'>
-          <div className='text-xl' data-testid='DistributionSection.Text'>
-            {t('DistributionSection.desc')}
-          </div>
+        <div className='text-xl' data-testid='DistributionSection.Text'>
+          {t('DistributionSection.desc')}
         </div>
-        <div className='mx-auto w-full md:w-3/5 space-y-16 mt-14' data-testid='DistributionSection.Chart'>
-          <Chart
+        <div className='mx-auto w-full lg:w-3/5 space-y-16 mt-14' data-testid='DistributionSection.Chart'>
+          <ChartSupply
             labelA={t('DistributionSection.chart.InitialSupply')}
             labelB={t('DistributionSection.chart.ToMasternodes')}
           />
           <div>
-            {t('DistributionSection.chart.desc')}
+            <Trans
+              t={t}
+              i18nKey='DistributionSection.chart.desc'
+              components={[
+                <ExternalLink key='0' className='text-primary-500' url='https://defiscan.live/transactions/c519fa1af8185186528dbfd5003e2ee110eae2f847b8d6143af220de6c837478' />,
+                <ExternalLink key='1' className='text-primary-500' url='https://github.com/DeFiCh/dfips/issues/17' />
+              ]}
+            />
           </div>
-          <Chart
-            labelA={t('DistributionSection.chart.DeFiChaiFoundation')}
-            labelB={t('DistributionSection.chart.ExternalPartners')}
+          <ChartDistribution
+            labelA={t('DistributionSection.chart.Airdropped')}
+            labelB={t('DistributionSection.chart.Burned')}
+            labelC={t('DistributionSection.chart.Destroyed')}
             fill
           />
         </div>
@@ -33,11 +41,11 @@ export function DistributionSection (): JSX.Element {
   )
 }
 
-function Chart (props: { fill?: boolean, labelA: string, labelB: string }): JSX.Element {
+function ChartSupply (props: { fill?: boolean, labelA: string, labelB: string }): JSX.Element {
   return (
     <>
-      <div className='w-full h-[32px] relative'>
-        <div className='w-full h-8 relative text-gray-500 text-2xl leading-[2.4rem]'>
+      <div className='w-full h-2 relative'>
+        <div className='w-full h-8 relative text-gray-500 text-xl'>
           <div className='w-[49%] left-0 absolute flex justify-center items-center'>
             <span className='inline-block px-2 bg-white relative z-[1000]'>49%</span>
             <span className='block h-[1px] w-[calc(100%_-_2px)] bg-gray-500 absolute top-[12px] left-[1px]' />
@@ -62,6 +70,52 @@ function Chart (props: { fill?: boolean, labelA: string, labelB: string }): JSX.
           className={classNames('w-[51%] h-full left-[49%] absolute flex justify-center items-center rounded-bl rounded-br ', props.fill !== undefined ? 'bg-primary-500 text-white' : 'bg-gray-200 text-black ')}
         >
           {props.labelB}
+        </div>
+      </div>
+    </>
+  )
+}
+
+function ChartDistribution (props: { fill?: boolean, labelA: string, labelB: string, labelC: string }): JSX.Element {
+  return (
+    <>
+      <div className='w-full relative'>
+        <div className='w-full h-2 relative text-gray-500 text-xl'>
+          <div className='w-[26%] left-0 absolute flex justify-center items-center'>
+            <span className='inline-block px-2 bg-white relative z-[1000]'>26%</span>
+            <span className='block h-[1px] w-[calc(100%_-_2px)] bg-gray-500 absolute top-[12px] left-[1px]' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] left-0' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] right-0' />
+          </div>
+          <div className='w-[27%] left-[26%] absolute flex justify-center items-center'>
+            <span className='inline-block px-2 bg-white relative z-[1000]'>27%</span>
+            <span className='block h-[1px] w-[calc(100%_-_2px)] bg-gray-500 absolute top-[12px] left-[1px]' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] left-0' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] right-0' />
+          </div>
+          <div className='w-[47%] left-[53%] absolute flex justify-center items-center'>
+            <span className='inline-block px-2 bg-white relative z-[1000]'>47%</span>
+            <span className='block h-[1px] w-[calc(100%_-_2px)] bg-gray-500 absolute top-[12px] left-[1px]' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] left-0' />
+            <span className='block w-[1px] h-[16px] bg-gray-500 absolute top-[4px] right-0' />
+          </div>
+        </div>
+      </div>
+      <div className={classNames('w-full h-[69px] relative', { 'divide-x': props.fill })}>
+        <div
+          className='w-[26%] h-full absolute left-0 flex items-center justify-center rounded-tl rounded-bl bg-primary-500 text-white'
+        >
+          {props.labelA}
+        </div>
+        <div
+          className={classNames('w-[27%] h-full left-[26%] absolute flex justify-center items-center rounded-bl rounded-br ', props.fill !== undefined ? 'bg-primary-500 text-white' : 'bg-gray-200 text-black ')}
+        >
+          {props.labelB}
+        </div>
+        <div
+          className={classNames('w-[47%] h-full left-[53%] absolute flex justify-center items-center rounded-bl rounded-br ', props.fill !== undefined ? 'bg-primary-500 text-white' : 'bg-gray-200 text-black ')}
+        >
+          {props.labelC}
         </div>
       </div>
     </>

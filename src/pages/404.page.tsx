2341ff7af21db2b63d7d69f1f5829d7d014fa222
Suraj Auwal <siradjiawoual@gmail.com>
2022-04-04 12:02:02 +0200

    `400` and  `500` page (#191)
    * Page Error component
    
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    * 404 error page
    
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    * 500 error page
    
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    * Translation added to error page
    
    Signed-off-by: Suraj Auwal <siradjiawoual@gmail.com>
    
    * Update public/locales/en-US/page-error.json
    
    Co-authored-by: Nichelle <8347352+nichellekoh@users.noreply.github.com>
    
    * Removed translation need for error pages
    
    Co-authored-by: Nichelle <8347352+nichellekoh@users.noreply.github.com>
    Co-authored-by: Joel-David Wong <git@joeldavid.dev>

---
+++ src/pages/404.page.tsx
@@ -0,0 +1,17 @@
+import { SSRConfig } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { PageError } from '@components/commons/error/PageError'
+
+export default function NotFound (): JSX.Element {
+  return (
+    <PageError title='Page Not Found - DeFiChain' errorDesc='404 - Page Not Found' text='BACK HOME' />
+  )
+}
+
+export async function getStaticProps ({ locale }): Promise<{ props: SSRConfig }> {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'layout']))
+    }
+  }
+}

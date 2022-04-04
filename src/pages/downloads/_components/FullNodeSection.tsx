a88faecdd2d350d3aaaa45443bce3c0bd999e32f
Nichelle <8347352+nichellekoh@users.noreply.github.com>
2022-04-04 17:29:48 +0800

    `Downloads`: Extract the latest versions from GitHub for defichain apps downloads (#197)
    * Extract the latest versions from GitHub for defichain apps downloads
    
    * Update modules.xml to point to correct iml file

--- src/pages/downloads/_components/FullNodeSection.tsx
+++ src/pages/downloads/_components/FullNodeSection.tsx
@@ -2,6 +2,7 @@ import { DownloadCard } from './DownloadCard'
 import { useTranslation } from 'next-i18next'
 import { ExternalLink } from '@components/commons/link/ExternalLink'
 import { BsFillTerminalFill } from 'react-icons/bs'
+import { DynamicDownloadCard } from './DynamicDownloadCard'
 import { FaReact } from 'react-icons/fa'
 
 export function FullNodeSection (): JSX.Element {
@@ -9,30 +10,31 @@ export function FullNodeSection (): JSX.Element {
 
   return (
     <>
-      <h2 className='text-xl lg:text-2xl font-medium mb-3 mt-10' data-testid='FullNodeSection.Title'>{t('FullNodeWallets.title')}</h2>
-      <p className='text-lg lg:text-xl mb-4' data-testid='FullNodeSection.Subtitle'>{t('FullNodeWallets.subtitle')}</p>
+      <h2
+        className='text-xl lg:text-2xl font-medium mb-3 mt-10'
+        data-testid='FullNodeSection.Title'
+      >{t('FullNodeWallets.title')}
+      </h2>
+      <p
+        className='text-lg lg:text-xl mb-4'
+        data-testid='FullNodeSection.Subtitle'
+      >{t('FullNodeWallets.subtitle')}
+      </p>
       <div className='flex flex-wrap pb-10 -m-1'>
-        <DownloadCard
+        <DynamicDownloadCard
           title={t('FullNodeWallets.entries.DFC.title')}
           desc={t('FullNodeWallets.entries.DFC.desc')}
           imageSrc='/assets/img/downloads/dfiwallet.png'
           testid='FullNodeWallets.Dfc'
-        >
-          <div className='flex flex-wrap items-center space-x-4 text-lg font-medium'>
-            <ExternalLink
-              text='macOS'
-              url='https://github.com/DeFiCh/app/releases/download/v2.10.0/DeFi-Wallet-2.10.0.dmg'
-            />
-            <ExternalLink
-              text='Windows'
-              url='https://github.com/DeFiCh/app/releases/download/v2.10.0/DeFi-Wallet-Setup-2.10.0.exe'
-            />
-            <ExternalLink
-              text='Linux (App Image)'
-              url='https://github.com/DeFiCh/app/releases/download/v2.10.0/DeFi-Wallet-2.10.0.AppImage'
-            />
-          </div>
-        </DownloadCard>
+          repoName='defiCh/app'
+          keywords={
+            {
+              mac: '.dmg',
+              win: '.exe',
+              linux: '.appimage'
+            }
+          }
+        />
 
         <DownloadCard
           title={t('FullNodeWallets.entries.RPI.title')}
@@ -48,45 +50,34 @@ export function FullNodeSection (): JSX.Element {
           </div>
         </DownloadCard>
 
-        <DownloadCard
+        <DynamicDownloadCard
           title={t('FullNodeWallets.entries.CLI.title')}
           desc={t('FullNodeWallets.entries.CLI.desc')}
           imageSrc={<BsFillTerminalFill fontSize={50} className='fill-primary-500' />}
           testid='FullNodeWallets.Cli'
-        >
-          <div className='flex items-center space-x-4 text-lg font-medium'>
-            <ExternalLink
-              text='macOS'
-              url='https://github.com/DeFiCh/ain/releases/download/v2.7.0/defichain-2.7.0-x86_64-apple-darwin18.tar.gz'
-            />
-            <ExternalLink
-              text='Windows'
-              url='https://github.com/DeFiCh/ain/releases/download/v2.7.0/defichain-2.7.0-x86_64-w64-mingw32.zip'
-            />
-            <ExternalLink
-              text='Linux'
-              url='https://github.com/DeFiCh/ain/releases/download/v2.7.0/defichain-2.7.0-x86_64-pc-linux-gnu.tar.gz'
-            />
-          </div>
-        </DownloadCard>
+          repoName='defiCh/ain'
+          keywords={
+            {
+              mac: 'apple',
+              win: 'w64',
+              linux: 'linux'
+            }
+          }
+        />
 
-        <DownloadCard
+        <DynamicDownloadCard
           title={t('FullNodeWallets.entries.DFIElectrum.title')}
           desc={t('FullNodeWallets.entries.DFIElectrum.desc')}
           imageSrc={<FaReact fontSize={50} className='fill-primary-500' />}
           testid='FullNodeWallets.DFIElectrum'
-        >
-          <div className='flex items-center space-x-4 text-lg font-medium'>
-            <ExternalLink
-              text='Windows'
-              url='https://github.com/BirthdayResearch/defichain-electrum/releases/download/0.0.1/defichain-electrum-0.0.1-setup.exe'
-            />
-            <ExternalLink
-              text='Linux'
-              url='https://github.com/BirthdayResearch/defichain-electrum/releases/download/0.0.1/defichain-electrum-0.0.1-x86_64.AppImage'
-            />
-          </div>
-        </DownloadCard>
+          repoName='BirthdayResearch/defichain-electrum'
+          keywords={
+            {
+              win: '.exe',
+              linux: '.appimage'
+            }
+          }
+        />
       </div>
     </>
   )

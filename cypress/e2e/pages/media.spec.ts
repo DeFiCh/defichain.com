f92325d4c2f51f9b3c918ff59acd3ae0d4acd54b
veralygit <57183851+veralygit@users.noreply.github.com>
2022-10-31 12:15:09 +0800

    update cypress e2e checks for media  downloads card length to 5

--- cypress/e2e/pages/media.spec.ts
+++ cypress/e2e/pages/media.spec.ts
@@ -1,57 +1,75 @@
-context('/media page on desktop', () => {
+context("/media page on desktop", () => {
   before(() => {
-    cy.visit('/media')
-  })
+    cy.visit("/media");
+  });
 
   beforeEach(() => {
-    cy.viewport('macbook-16')
-  })
+    cy.viewport("macbook-16");
+  });
 
-  it('should have Header', () => {
-    cy.findByTestId('Header.title').should('have.text', 'Media')
-    cy.findByTestId('Header.desc.main').should('have.text', 'Media resources and assets.')
-  })
+  it("should have Header", () => {
+    cy.findByTestId("Header.title").should("have.text", "Media");
+    cy.findByTestId("Header.desc.main").should(
+      "have.text",
+      "Media resources and assets."
+    );
+  });
 
-  it('should have MediaSection', function () {
-    cy.findByTestId('MediaSection').within(() => {
-      cy.findByTestId('Section.Title').should('have.text', 'DeFiChain covered in the press and media.')
-      cy.findAllByTestId('MediaSection.Card').should('have.length.at.least', 20)
-    })
-  })
+  it("should have MediaSection", () => {
+    cy.findByTestId("MediaSection").within(() => {
+      cy.findByTestId("Section.Title").should(
+        "have.text",
+        "DeFiChain covered in the press and media."
+      );
+      cy.findAllByTestId("MediaSection.Card").should(
+        "have.length.at.least",
+        20
+      );
+    });
+  });
 
-  it('should have DownloadSection', function () {
-    cy.findByTestId('DownloadSection').within(() => {
-      cy.findByTestId('Section.Title').should('have.text', 'Downloads')
-      cy.findAllByTestId('DownloadSection.Card').should('have.length', 4)
-    })
-  })
-})
+  it("should have DownloadSection", () => {
+    cy.findByTestId("DownloadSection").within(() => {
+      cy.findByTestId("Section.Title").should("have.text", "Downloads");
+      cy.findAllByTestId("DownloadSection.Card").should("have.length", 5);
+    });
+  });
+});
 
-context('/media page on mobile', () => {
+context("/media page on mobile", () => {
   before(() => {
-    cy.visit('/media')
-  })
+    cy.visit("/media");
+  });
 
   beforeEach(() => {
-    cy.viewport('iphone-x')
-  })
+    cy.viewport("iphone-x");
+  });
 
-  it('should have Header', () => {
-    cy.findByTestId('Header.title').should('have.text', 'Media')
-    cy.findByTestId('Header.desc.main').should('have.text', 'Media resources and assets.')
-  })
+  it("should have Header", () => {
+    cy.findByTestId("Header.title").should("have.text", "Media");
+    cy.findByTestId("Header.desc.main").should(
+      "have.text",
+      "Media resources and assets."
+    );
+  });
 
-  it('should have MediaSection', function () {
-    cy.findByTestId('MediaSection').within(() => {
-      cy.findByTestId('Section.Title').should('have.text', 'DeFiChain covered in the press and media.')
-      cy.findAllByTestId('MediaSection.Card').should('have.length.at.least', 20)
-    })
-  })
+  it("should have MediaSection", () => {
+    cy.findByTestId("MediaSection").within(() => {
+      cy.findByTestId("Section.Title").should(
+        "have.text",
+        "DeFiChain covered in the press and media."
+      );
+      cy.findAllByTestId("MediaSection.Card").should(
+        "have.length.at.least",
+        20
+      );
+    });
+  });
 
-  it('should have DownloadSection', function () {
-    cy.findByTestId('DownloadSection').within(() => {
-      cy.findByTestId('Section.Title').should('have.text', 'Downloads')
-      cy.findAllByTestId('DownloadSection.Card').should('have.length', 4)
-    })
-  })
-})
+  it("should have DownloadSection", () => {
+    cy.findByTestId("DownloadSection").within(() => {
+      cy.findByTestId("Section.Title").should("have.text", "Downloads");
+      cy.findAllByTestId("DownloadSection.Card").should("have.length", 5);
+    });
+  });
+});

35633fd975283c4f44e0d00cf4b8f271e2fca05e
G9000 <leo.caesar@live.com>
2022-11-24 12:41:32 +0800

    fix e2e

--- cypress/e2e/pages/media.spec.ts
+++ cypress/e2e/pages/media.spec.ts
@@ -27,13 +27,6 @@ context("/media page on desktop", () => {
       );
     });
   });
-
-  it("should have DownloadSection", () => {
-    cy.findByTestId("DownloadSection").within(() => {
-      cy.findByTestId("Section.Title").should("have.text", "Downloads");
-      cy.findAllByTestId("DownloadSection.Card").should("have.length", 5);
-    });
-  });
 });
 
 context("/media page on mobile", () => {

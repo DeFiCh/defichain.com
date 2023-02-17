08456f3c569fd0f04353c1227912192d385793dd
veralygit <57183851+veralygit@users.noreply.github.com>
2023-02-17 19:16:59 +0800

    feat(ui-ux): add Buttons 2.0 component

--- src/components/commons/Buttons.tsx
+++ src/components/commons/Buttons.tsx
@@ -1,24 +1,108 @@
 import { PropsWithChildren } from "react";
 import classNames from "classnames";
 
-/**
- * Container implementation that is part of the design language.
- */
-export function Button(
-  props: PropsWithChildren<{ className?: string; text: string }>
-): JSX.Element {
+export function Button({
+  buttonText,
+  className,
+  disabled,
+  href,
+  onClick,
+  isCTAButton = true,
+}: {
+  buttonText: string;
+  className?: string;
+  disabled?: boolean;
+  href?: string;
+  onClick?: () => void;
+  isCTAButton?: boolean;
+}) {
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={classNames({ "pointer-events-none": disabled })}
+      >
+        <ButtonElement
+          buttonText={buttonText}
+          className={className}
+          disabled={disabled}
+          onClick={onClick}
+          isCTAButton={isCTAButton}
+        />
+      </a>
+    );
+  }
+  return (
+    <ButtonElement
+      buttonText={buttonText}
+      className={className}
+      disabled={disabled}
+      onClick={onClick}
+      isCTAButton={isCTAButton}
+    />
+  );
+}
+
+function ButtonElement({
+  buttonText,
+  className,
+  disabled,
+  onClick,
+  isCTAButton = true,
+}: {
+  buttonText: string;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  isCTAButton?: boolean;
+}) {
   return (
     <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
       className={classNames(
-        "button rounded border border-primary-500 py-2 px-6 font-medium text-white bg-primary-500 hover:bg-primary-600",
-        props.className
+        className ?? "py-4 px-28",
+        "flex items-center justify-center rounded-[92px] font-bold disabled:opacity-30 disabled:pointer-events-none",
+        isCTAButton
+          ? "bg-dark-1000 text-dark-100 hover:text-dark-00 active:text-dark-00 hover:cta-on-hover active:cta-on-press"
+          : "text-dark-1000 hover:on-button-hover active:on-button-press"
       )}
     >
-      {props.text}
+      {buttonText}
     </button>
   );
 }
 
+export function GradientButton({
+  buttonText,
+  className,
+  disabled,
+  onClick,
+}: {
+  buttonText: string;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className="accent-gradient-1 rounded-[44px] p-1 w-auto h-auto"
+    >
+      <div
+        className={classNames(
+          "flex items-center bg-dark-00 text-light-00 rounded-[44px] hover:accent-gradient-1 w-full h-full disabled:opacity-30 disabled:pointer-events-none whitespace-nowrap",
+          className ?? "px-5 py-3"
+        )}
+      >
+        {buttonText}
+      </div>
+    </button>
+  );
+}
+// To remove currently not removed yet to load homescreen without error
 export function ButtonUnfilled(
   props: PropsWithChildren<{ className?: string; text: string }>
 ): JSX.Element {

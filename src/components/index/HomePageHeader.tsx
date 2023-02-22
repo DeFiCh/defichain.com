de163d269be6970af165353eb99894a20294cc59
nattadex <elocinnat99@gmail.com>
2023-02-23 00:40:00 +0800

    hero banner

--- src/components/index/HomePageHeader.tsx
+++ src/components/index/HomePageHeader.tsx
@@ -1,34 +1,42 @@
 import { Container } from "@components/commons/Container";
 import { useTranslation } from "next-i18next";
+import Image from "next/image";
+import dfiCoinGif from "../../../public/assets/svg/hero/dfi-coin-gif.gif";
 
 export function HomePageHeader(): JSX.Element {
   const { t } = useTranslation("page-index");
+  const separatedTitle = t("Header.title").split(" ");
+  const firstWord = separatedTitle[0];
+  separatedTitle.shift();
 
   return (
-    <div
-      className="flex flex-col items-center pb-24"
-      style={{
-        backgroundImage: "url('/assets/svg/hero/fortcanning.svg')",
-        backgroundSize: "cover",
-        backgroundPosition: "right center",
-      }}
-    >
-      <Container className="flex w-full min-h-[35rem] lg:min-h-[45rem] items-center justify-center">
-        <div className="w-full md:w-3/4 -mt-48">
+    <div className="flex flex-col items-center pb-24 mt-9" style={{}}>
+      <Container className="flex items-center justify-between w-[1242.76px] lg:px-0">
+        <div className="w-[479px]">
           <h1
-            className="w-full text-3xl lg:text-6xl text-center mb-6 font-medium"
+            className="w-full text-[3xl] lg:text-[80px] lg:leading-[84px]"
             data-testid="Header.title"
           >
-            {t("Header.title")}
+            <span className="text-brand-100">{firstWord} </span>
+            {separatedTitle.join(" ")}
           </h1>
-          <div className="mt-10">
+          <div className="mt-5">
             <div
-              className="w-full text-lg md:text-xl text-center"
+              className="w-full text-lg md:text-xl text-dark-700"
               data-testid="Header.desc.main"
             >
-              {t("Header.desc")}
+              <span>{t("Header.desc")}</span>
             </div>
           </div>
+        </div>
+        <div>
+          <Image
+            src={dfiCoinGif}
+            alt="DFI coin gif"
+            width={595}
+            height={590}
+            objectFit="cover"
+          />
         </div>
       </Container>
     </div>

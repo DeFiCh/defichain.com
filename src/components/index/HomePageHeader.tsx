07a8aab694e500189dc6df5e5da0765f514f644d
nattadex <elocinnat99@gmail.com>
2023-02-23 13:31:11 +0800

    some styling changes for responsiveness

--- src/components/index/HomePageHeader.tsx
+++ src/components/index/HomePageHeader.tsx
@@ -10,26 +10,31 @@ export function HomePageHeader(): JSX.Element {
   separatedTitle.shift();
 
   return (
-    <div className="flex flex-col items-center pb-24 mt-9" style={{}}>
-      <Container className="flex items-center justify-between w-[1242.76px] lg:px-0">
-        <div className="w-[479px]">
+    <div
+      className="flex flex-col items-center pb-24 mt-[15.87px] lg:mt-9"
+      style={{}}
+    >
+      <Container className="flex flex-col-reverse lg:flex-row lg:justify-between px-6 items-start lg:pl-[120px] lg:pr-[77.24px] max-w-full">
+        <div className="lg:w-[479px] lg:mt-16">
           <h1
-            className="w-full text-[3xl] lg:text-[80px] lg:leading-[84px]"
+            className="w-full text-4xl lg:text-[80px] lg:leading-[84px]"
             data-testid="Header.title"
           >
-            <span className="text-brand-100">{firstWord} </span>
+            <span className="text-brand-100">
+              {firstWord} <br />
+            </span>
             {separatedTitle.join(" ")}
           </h1>
           <div className="mt-5">
             <div
-              className="w-full text-lg md:text-xl text-dark-700"
+              className="w-full lg:text-lg text-dark-700"
               data-testid="Header.desc.main"
             >
               <span>{t("Header.desc")}</span>
             </div>
           </div>
         </div>
-        <div>
+        <div className="w-[252.82px] lg:w-[595px] ml-[83px] lg:ml-auto">
           <Gif src={dfiCoinGif} alt="DFI coin gif" width={595} height={590} />
         </div>
       </Container>

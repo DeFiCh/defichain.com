155b451118497374bf857df3a4890819923220ed
nattadex <elocinnat99@gmail.com>
2023-02-27 17:01:06 +0800

    add max width for super large screens

--- src/components/index/HomePageHeader.tsx
+++ src/components/index/HomePageHeader.tsx
@@ -10,7 +10,7 @@ export function HomePageHeader(): JSX.Element {
 
   return (
     <div className="flex flex-col items-center pb-24 md:mt-0">
-      <Container className="flex flex-col-reverse px-6 items-start max-w-full md:flex-row md:justify-between md:pl-10 md:pr-0 lg:pl-12 lg:pr-0 xl:pl-[120px] xl:pr-[59px] 2xl:pl-[300px] 2xl:pr-[39px]">
+      <Container className="flex flex-col-reverse px-6 items-start max-w-full md:flex-row md:justify-between md:pl-10 md:pr-0 lg:pl-12 lg:pr-0 xl:pl-[120px] xl:pr-[59px] 2xl:pl-[300px] 2xl:pr-[39px] 2xl:max-w-[1920px]">
         <div className="mt-[-41px] md:w-[316px] md:mt-[92px] lg:max-w-none lg:w-[479px] xl:mt-[120px] 2xl:mt-[148px]">
           <h1
             className="w-full text-5xl leading-[52px] lg:text-[80px] lg:leading-[84px]"
@@ -30,7 +30,7 @@ export function HomePageHeader(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="w-full min-w-[364px] max-w-[379px] mt-[-32px] mr-[-16px] ml-auto md:mr-[-32.18px] md:ml-0 md:min-w-[382px] md:max-w-[584px] lg:ml-auto lg:max-w-[722px] lg:mt-[-49px] xl:max-w-[758px] xl:mt-[-57px] 2xl:max-w-[929px] 2xl:mt-[-80px]">
+        <div className="w-full min-w-[364px] max-w-[379px] mt-[-31px] mr-[-16px] ml-auto md:mt-[-49px] md:mr-[-32.18px] md:ml-0 md:min-w-[382px] md:max-w-[584px] lg:ml-auto lg:max-w-[722px] lg:mt-[-49px] xl:max-w-[758px] xl:mt-[-57px] 2xl:max-w-[929px] 2xl:mt-[-80px]">
           <Video
             src="/assets/svg/hero/dfi-coin-video.mp4"
             width={595}

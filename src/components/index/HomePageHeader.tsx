85a74ae0b9b16d3dd79819678037793ea7fb225c
nattadex <elocinnat99@gmail.com>
2023-02-27 16:15:31 +0800

    design according to responsive design

--- src/components/index/HomePageHeader.tsx
+++ src/components/index/HomePageHeader.tsx
@@ -9,11 +9,11 @@ export function HomePageHeader(): JSX.Element {
   separatedTitle.shift();
 
   return (
-    <div className="flex flex-col items-center pb-24 mt-[15.87px] lg:mt-9">
-      <Container className="flex flex-col-reverse lg:flex-row lg:justify-between px-6 items-start lg:pl-[120px] lg:pr-[77.24px] max-w-full">
-        <div className="lg:w-[479px] lg:mt-16 mt-[25.44px]">
+    <div className="flex flex-col items-center pb-24 md:mt-0">
+      <Container className="flex flex-col-reverse md:flex-row md:justify-between px-6 items-start md:pl-10 md:pr-0 lg:pl-12 lg:pr-0 xl:pl-[120px] xl:pr-[59px] 2xl:pl-[300px] 2xl:pr-[39px] max-w-full">
+        <div className="md:max-w-[403px] md:mt-[92px] lg:max-w-none lg:w-[479px] xl:mt-[120px] 2xl:mt-[148px]">
           <h1
-            className="w-full text-4xl lg:text-[80px] lg:leading-[84px]"
+            className="w-full text-5xl leading-[52px] lg:text-[80px] lg:leading-[84px]"
             data-testid="Header.title"
           >
             <span className="text-brand-100">
@@ -30,7 +30,7 @@ export function HomePageHeader(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="w-[252.82px] lg:w-[595px] ml-[83px] lg:ml-auto">
+        <div className="w-full min-w-[364px] max-w-[379px] mt-[-32px] mr-[-41px] md:ml-0 md:min-w-[382px] md:max-w-[584px] md:mt-[-28px] lg:max-w-[722px] lg:mt-[-49px] xl:max-w-[758px] xl:mt-[-57px] 2xl:max-w-[929px] 2xl:mt-[-80px] ml-auto lg:ml-auto">
           <Video
             src="/assets/svg/hero/dfi-coin-video.mp4"
             width={595}

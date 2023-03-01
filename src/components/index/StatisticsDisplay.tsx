18df7b5397ad533869c1a539bd2209d909f1c1c8
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-01 13:55:16 +0800

    update tablet and mobile view

--- src/components/index/StatisticsDisplay.tsx
+++ src/components/index/StatisticsDisplay.tsx
@@ -6,6 +6,7 @@ import {
 } from "@defichain/whale-api-client/dist/api/stats";
 import { useEffect, useState } from "react";
 import CountUp from "react-countup";
+import classNames from "classnames";
 import { useWhaleApiClient } from "../../layouts/context/WhaleContext";
 import { calculatePercentage } from "../../shared/calculatePercentage";
 
@@ -35,7 +36,7 @@ export function StatsDisplay() {
         <div className="card-bg rounded-[30px] p-6">
           <div className="flex lg:flex-row flex-col gap-y-4 justify-evenly justify-center">
             <StatsItem
-              title="Total DFI Minted"
+              title="TOTAL DFI MINTED"
               stats={supply?.total ?? 0}
               desc={`${calculatePercentage(
                 supply?.total,
@@ -43,10 +44,11 @@ export function StatsDisplay() {
               )} of max supply`}
             />
             <StatsItem
-              title="Total value locked in usd"
+              title="Total VALUE LOCKED IN USD"
               prefix="$"
               stats={stats?.tvl.total ?? 0}
-              desc="DEX | Masternodes | Vaults"
+              desc="Pool | Masternodes | Vaults"
+              descStyle="lg:block hidden"
             />
             <StatsItem
               title="MASTERNODES"
@@ -67,11 +69,13 @@ function StatsItem({
   stats,
   desc,
   prefix,
+  descStyle,
 }: {
   title: string;
   stats: number;
   desc: string;
   prefix?: string;
+  descStyle?: string;
 }) {
   const suffix = useUnitSuffix(stats.toString(), UnitSuffixReturnValue.SUFFIX);
   const value = useUnitSuffix(stats.toString(), UnitSuffixReturnValue.VALUE);
@@ -81,20 +85,23 @@ function StatsItem({
       <div className="bg-clip-text text-transparent accent-gradient-2 font-bold leading-5">
         {title}
       </div>
-      <div className="text-dark-1000 lg:text-[52px] text-xl leading-6 lg:leading-[52px] lg:grow-0 grow text-right">
-        {prefix ?? ""}
-        <CountUp
-          onUpdate={({ reset, start }) => {
-            reset();
-            start();
-          }}
-          end={Number(value)}
-          enableScrollSpy
-          duration={0.5}
-        />
-        {suffix ? suffix.toString().concat("+") : ""}
+      <div className="flex flex-col lg:items-center items-end lg:gap-y-2 gap-y-1 lg:grow-0 grow">
+        <div className="text-dark-1000 lg:text-[52px] text-xl leading-6 lg:leading-[52px]">
+          {prefix ?? ""}
+          <CountUp
+            onUpdate={({ reset, start }) => {
+              reset();
+              start();
+            }}
+            end={Number(value)}
+            enableScrollSpy
+            duration={0.5}
+          />
+          {suffix ? suffix.toString().concat("+") : ""}
+        </div>
+
+        <div className={classNames("text-dark-700", descStyle)}>{desc}</div>
       </div>
-      <div className=" hidden lg:block text-dark-700">{desc}</div>
     </div>
   );
 }

a9bd38e3017c1507d61a684d31d11352030f0643
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-02 14:33:40 +0800

    hide start exploring button in mobile view

--- src/components/index/StartExploring.tsx
+++ src/components/index/StartExploring.tsx
@@ -3,7 +3,7 @@ import { Container } from "@components/commons/Container";
 
 export function StartExploring() {
   return (
-    <Container className="flex justify-center md:mt-0 mt-9">
+    <Container className="flex justify-center md:mt-0 mt-9 hidden md:flex">
       <a href="#statistics_display">
         <button className="group flex flex-col items-center gap-y-[17px]">
           <div className="text-sm text-dark-700 group-hover:text-brand-100">

d0d949a0a895b853e9d3bbd102f7e8bbfe8a4e0a
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-10 15:46:54 +0800

    update stylings for mobile view and update variable namings

--- src/pages/explore/_components/ExploreCards.tsx
+++ src/pages/explore/_components/ExploreCards.tsx
@@ -18,35 +18,33 @@ export function ExploreCards({
   href: string;
 }) {
   const dimension = useWindowDimensions();
-  const shiftMobileView = dimension.width <= 800;
+  const mobileView = dimension.width <= 800;
   const [isHoverIn, setIsHoverIn] = useState(false);
 
   const bgMapping = {
     metalCoin: [
       "url(/assets/img/explore/metal-dfi-coin.png)",
-      shiftMobileView
+      mobileView
         ? "bg-[right_-10em_top_-6em]"
         : "bg-[right_-6em_bottom_-1.8em]",
     ],
     arrow: [
       "url(/assets/img/explore/arrow.png)",
-      shiftMobileView ? "bg-[right_top_-6em]" : "bg-right-bottom",
+      mobileView ? "bg-[right_top_-6em]" : "bg-right-bottom",
     ],
     diamondCoin: [
       "url(/assets/img/explore/diamond-coin.png)",
-      shiftMobileView
+      mobileView
         ? "bg-[right_-16em_top_-25em]"
         : "bg-[right_-12em_bottom_-18em]",
     ],
     phone: [
       "url(/assets/img/explore/phone.png)",
-      shiftMobileView
-        ? "bg-[right_-2.5em_top_-10em]"
-        : "bg-[right_bottom_-3.5em]",
+      mobileView ? "bg-[right_-2.5em_top_-10em]" : "bg-[right_bottom_-3.5em]",
     ],
     cube: [
       "url(/assets/img/explore/cube.png)",
-      shiftMobileView ? "bg-[right_10%_top_-2em]" : "bg-right-bottom",
+      mobileView ? "bg-[right_10%_top_-2em]" : "bg-right-bottom",
     ],
   };
 
@@ -59,7 +57,8 @@ export function ExploreCards({
         setIsHoverIn(false);
       }}
       className={classNames(
-        "p-[1px] rounded-[15px] h-[300px] z-10",
+        "rounded-[15px] h-[300px] z-10",
+        mobileView ? "p-[0.5px]" : "p-[1px]",
         customSize ?? "md:w-full min-w-[272px]",
         "cursor-pointer",
         "card-outline-2 hover:accent-gradient-1 transition-all ease-in-out duration-300"
@@ -81,16 +80,19 @@ export function ExploreCards({
           <SectionTitle text={title} />
           <div
             className={classNames(
-              "text-light-00 inline mt-2  ",
-              shiftMobileView
+              "text-light-00 inline mt-2",
+              mobileView
                 ? "text-xl leading-6"
                 : "text-[32px] leading-10 max-w-[268px]"
             )}
           >
             {desc}
-            {(shiftMobileView || isHoverIn) && (
-              <button className="md:ml-[18px] ml-[12.17px] align-middle">
-                <IoArrowForward size={20} className="" />
+            {(mobileView || isHoverIn) && (
+              <button
+                type="button"
+                className="md:ml-[18px] ml-[12.17px] align-middle"
+              >
+                <IoArrowForward size={20} />
               </button>
             )}
           </div>

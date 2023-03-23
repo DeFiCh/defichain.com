6883bc52ca2d5ccf6bb3716f1804d322962ced96
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-23 15:40:22 +0800

    feat(ui-ux): add daily use and advance use sections to explore wallets page (#536)
    * feat(ui-ux): add explore wallets advance section
    
    * update cards hover states
    
    * add backdrop blur
    
    * update stylings and add for daily use section
    
    * update mappings
    
    * fix stylings for cards and logic for dynamic download cards
    
    * chore(explore): update explore wallets
    
    * chore(cleanup): remove unused images
    
    * update image width and height to 72
    
    * update react icon to height 72 and width 63
    
    * updated spacing for tablet and mobile view between cardlinks
    
    ---------
    
    Co-authored-by: Keng Ye <40191153+kyleleow@users.noreply.github.com>
    Co-authored-by: Lyka Labrada <lykalabrada@gmail.com>

---
+++ src/pages/explore/wallets/_components/AdvanceUsageSection.tsx
@@ -0,0 +1,95 @@
+import { useTranslation } from "next-i18next";
+import { FaReact } from "react-icons/fa";
+import { Container } from "@components/commons/Container";
+import { SectionTitle } from "@components/commons/SectionTitle";
+import classNames from "classnames";
+import { DynamicDownloadCard } from "./DynamicDownloadCard";
+import { DownloadCard } from "./DownloadCard";
+import { CardLink, IconType } from "./CardLink";
+
+export function AdvanceUsageSection() {
+  const { t } = useTranslation(["page-explore-wallets"]);
+
+  return (
+    <Container className="mt-[120px] md:mt-[96px] lg:mt-[240px] lg:mb-[240px] md:mb-[112px] mb-[72px]">
+      <div className="flex flex-col gap-y-5">
+        <div className="flex md:justify-center justify-start">
+          <SectionTitle text="SUITABLE FOR ADVANCE USAGE" />
+        </div>
+
+        <h2
+          className={classNames(
+            "lg:text-[60px] lg:leading-[72px] md:text-[40px] md:leading-[44px] text-[32px] leading-[36px]",
+            "text-dark-1000 md:text-center text-left"
+          )}
+          data-testid="AdvanceUseSection.Title"
+        >
+          {t("advanceUseSection.title")}
+        </h2>
+        <p
+          className={classNames(
+            "lg:text-xl text-base font-desc",
+            "lg:tracking-normal tracking-[0.03em]",
+            "text-dark-700 md:text-center text-left"
+          )}
+          data-testid="AdvanceUseSection.Subtitle"
+        >
+          {t("advanceUseSection.subtitle")}
+        </p>
+      </div>
+
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-6 gap-5 md:mt-16 mt-8">
+        <DynamicDownloadCard
+          title={t("advanceUseSection.entries.DFC.title")}
+          desc={t("advanceUseSection.entries.DFC.desc")}
+          imageSrc="/assets/img/explore/explore-wallet-dfiwallet.png"
+          testid="AdvanceUseSection.Dfc"
+          repoName="defiCh/app"
+          keywords={{
+            mac: ".dmg",
+            win: ".exe",
+            linux: ".appimage",
+          }}
+        />
+
+        <DownloadCard
+          title={t("advanceUseSection.entries.RPI.title")}
+          desc={t("advanceUseSection.entries.RPI.desc")}
+          imageSrc="/assets/img/explore/explore-wallet-dfinode.png"
+          testid="AdvanceUseSection.Rpi"
+        >
+          <CardLink
+            url="https://github.com/Martin8617/Defi-Wallet-for-Raspberry-Pi"
+            descText="View on"
+            text={IconType.GITHUB}
+          />
+        </DownloadCard>
+
+        <DynamicDownloadCard
+          title={t("advanceUseSection.entries.CLI.title")}
+          desc={t("advanceUseSection.entries.CLI.desc")}
+          imageSrc="/assets/img/explore/explore-wallet-cli.png"
+          testid="AdvanceUseSection.Cli"
+          repoName="defiCh/ain"
+          keywords={{
+            mac: "apple",
+            win: "w64",
+            linux: "linux",
+          }}
+        />
+
+        <DynamicDownloadCard
+          title={t("advanceUseSection.entries.DFIElectrum.title")}
+          desc={t("advanceUseSection.entries.DFIElectrum.desc")}
+          imageSrc={<FaReact className="fill-brand-100 w-[72px] h-[63px]" />}
+          testid="AdvanceUseSection.DFIElectrum"
+          repoName="BirthdayResearch/defichain-electrum"
+          keywords={{
+            win: ".exe",
+            linux: ".appimage",
+          }}
+        />
+      </div>
+    </Container>
+  );
+}

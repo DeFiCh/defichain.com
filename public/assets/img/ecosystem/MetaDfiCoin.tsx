54a0c26a9b61e2f3cbc31c880d4a01424dbe8586
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-03-23 18:07:27 +0800

    fix: handle hover ellipse bg

--- public/assets/img/ecosystem/MetaDfiCoin.tsx
+++ public/assets/img/ecosystem/MetaDfiCoin.tsx
@@ -5,12 +5,10 @@ import MetaDFICoinEllipse from "./meta-dfi-coin-ellipse.png";
 
 interface MetaDfiCoinProps {
   id: string;
-  className?: string;
 }
 
 export function MetaDfiCoin(props: MetaDfiCoinProps): JSX.Element {
   const { id } = props;
-  console.log(`${id}_ellipse`);
   return (
     <div>
       <Image
@@ -25,5 +23,9 @@ export function MetaDfiCoin(props: MetaDfiCoinProps): JSX.Element {
 
 export function MetaDfiCoinHover(props: MetaDfiCoinProps): JSX.Element {
   const { id } = props;
-  return <Image src={MetaDFICoinIconHover} alt="Meta DFI Coin Icon" id={id} />;
+  return (
+    <div>
+      <Image src={MetaDFICoinIconHover} alt="Meta DFI Coin Icon" id={id} />
+    </div>
+  );
 }

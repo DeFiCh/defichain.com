0472900c5a7915616c361ab1d707e3d5451b478a
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-24 14:47:16 +0800

    updated font size for hero banner desc

--- src/pages/explore/dfi/index.page.tsx
+++ src/pages/explore/dfi/index.page.tsx
@@ -5,16 +5,14 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 export default function ExploreDFI() {
   const { t } = useTranslation("page-explore-dfi");
   return (
-    <div>
-      <HeroBanner
-        title={t("heroBanner.title")}
-        subtitle={t("heroBanner.subtitle")}
-        desc={t("heroBanner.desc")}
-        heroBg={HeroBannerBg.DFI_COIN}
-        hasStartExploringButton
-        startExploringJumpLink="/"
-      />
-    </div>
+    <HeroBanner
+      title={t("heroBanner.title")}
+      subtitle={t("heroBanner.subtitle")}
+      desc={t("heroBanner.desc")}
+      heroBg={HeroBannerBg.DFI_COIN}
+      hasStartExploringButton
+      startExploringJumpLink="/"
+    />
   );
 }
 

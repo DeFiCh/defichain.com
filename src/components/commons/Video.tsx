3cf46fa92b1240de94b29fbe59bc50ce40173e75
veralygit <57183851+veralygit@users.noreply.github.com>
2023-03-30 14:41:40 +0800

    add video loop

--- src/components/commons/Video.tsx
+++ src/components/commons/Video.tsx
@@ -9,7 +9,7 @@ interface VideoProps {
 export function Video(props: VideoProps): JSX.Element {
   const { src, width, height, poster, id } = props;
   return (
-    <video id={id} poster={poster} autoPlay muted playsInline>
+    <video id={id} poster={poster} autoPlay muted playsInline loop>
       <source src={src} type="video/mp4" width={width} height={height} />
     </video>
   );

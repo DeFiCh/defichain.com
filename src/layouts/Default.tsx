a909132237ce541d1b9d32321da7a00b121c9315
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-04-03 22:37:56 +0800

    fix(ui-ux): generic background for all pages except homepage

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import { PropsWithChildren, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import classNames from "classnames";
 import Footer from "./components/Footer";
 import { Header } from "./components/Header";
 import { WhaleProvider } from "./context/WhaleContext";
@@ -16,6 +17,10 @@ const description =
 export function Default(props: PropsWithChildren<any>): JSX.Element | null {
   const router = useRouter();
   const [mounted, setMounted] = useState(false);
+  const isGeneric = router.pathname === "/" ? "" : "generic-";
+  const bgPicture = isGeneric
+    ? "bg-fixed bg-[url(/assets/img/background/generic-background-768.png)] md:bg-[url(/assets/img/background/generic-background-768.png)] lg:bg-[url(/assets/img/background/generic-background-1024.png)] xl:bg-[url(/assets/img/background/generic-background-1440.png)] 2xl:bg-[url(/assets/img/background/generic-background-1920.png)]"
+    : "bg-[url(/assets/img/background/background-768.png)] md:bg-[url(/assets/img/background/background-1024.png)] lg:bg-[url(/assets/img/background/background-1440.png)] xl:bg-[url(/assets/img/background/background-1536.png)] 2xl:bg-[url(/assets/img/background/background-1920.png)]";
 
   useEffect(() => {
     setMounted(true);
@@ -78,8 +83,13 @@ export function Default(props: PropsWithChildren<any>): JSX.Element | null {
           <div className="bg-dark-00 relative">
             <Header />
             <main className="flex-grow text-dark-1000">{props.children}</main>
+            <div
+              className={classNames(
+                "absolute z-0 bg-top bg-no-repeat inset-0",
+                bgPicture
+              )}
+            />
             <Footer />
-            <div className="absolute z-0 bg-top bg-no-repeat inset-0 bg-[url(/assets/img/background/background-768.png)] md:bg-[url(/assets/img/background/background-1024.png)] lg:bg-[url(/assets/img/background/background-1440.png)] xl:bg-[url(/assets/img/background/background-1536.png)] 2xl:bg-[url(/assets/img/background/background-1920.png)]" />
           </div>
         </WhaleProvider>
       )}

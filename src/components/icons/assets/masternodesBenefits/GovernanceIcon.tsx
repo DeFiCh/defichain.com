5bb4c014252a0d60fa762f7195d88c38c2e8a79f
nattadex <elocinnat99@gmail.com>
2023-04-04 09:59:42 +0800

    removed animated svg on hover ; deprioritized

--- src/components/icons/assets/masternodesBenefits/GovernanceIcon.tsx
+++ src/components/icons/assets/masternodesBenefits/GovernanceIcon.tsx
@@ -44,102 +44,30 @@ export function GovernanceIconHover(props: SVGProps<SVGSVGElement>) {
   const { className } = props;
   return (
     <svg
-      width="64"
+      width="52"
       height="51"
-      viewBox="0 0 64 51"
+      viewBox="0 0 52 51"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
       <path
-        d="M38.3332 0.0909424L63.3332 25.0909L38.3332 50.0909L13.3333 25.0909L38.3332 0.0909424Z"
-        fill="url(#paint0_linear_5602_70145)"
-      >
-        <animate
-          attributeName="d"
-          attributeType="XML"
-          from="M38.3332 0.0909424L63.3332 25.0909L38.3332 50.0909L13.3333 25.0909L38.3332 0.0909424Z"
-          to="M-8.60982e-07 30.303L19.697 10.6061L39.3939 30.303L19.697 50L-8.60982e-07 30.303Z"
-          begin="0s"
-          dur="0.3s"
-          fill="freeze"
-        />
-      </path>
-      <ellipse
-        cx="14.1664"
-        cy="25.9242"
-        rx="14.1667"
-        ry="14.1667"
-        fill="url(#paint1_linear_5602_70145)"
-      >
-        <animate
-          attributeName="cx"
-          attributeType="XML"
-          from="14.1664"
-          to="40.1515"
-          begin="0s"
-          dur="0.3s"
-          fill="freeze"
-        />
-        <animate
-          attributeName="cy"
-          attributeType="XML"
-          from="25.9242"
-          to="11.3636"
-          begin="0s"
-          dur="0.3s"
-          fill="freeze"
-        />
-        <animate
-          attributeName="r"
-          attributeType="XML"
-          from="14.1667"
-          to="11.3636"
-          begin="0s"
-          dur="0.3s"
-          fill="freeze"
-        />
-        {/* <animate
-          attributeName="fill"
-          from="url(#paint1_linear_5602_70145)"
-          to="gradient_after_animation2"
-          begin="0s"
-          dur="0.3s"
-          fill="freeze"
-        /> */}
-      </ellipse>
+        d="M-8.60982e-07 30.394L19.697 10.697L39.3939 30.394L19.697 50.0909L-8.60982e-07 30.394Z"
+        fill="url(#paint0_linear_5105_60482)"
+      />
+      <circle
+        cx="40.1515"
+        cy="11.4546"
+        r="11.3636"
+        fill="url(#paint1_linear_5105_60482)"
+      />
       <defs>
         <linearGradient
-          id="paint0_linear_5602_70145"
-          x1="3.17701"
-          y1="-18.1382"
-          x2="54.2567"
-          y2="-34.1757"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#EC008C" />
-          <stop offset="0.846071" stopColor="#5B10FF" />
-          <stop offset="1" stopColor="#0468D6" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_5602_70145"
-          x1="-5.75545"
-          y1="1.42771"
-          x2="23.1897"
-          y2="-7.66017"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#EC008C" />
-          <stop offset="0.846071" stopColor="#5B10FF" />
-          <stop offset="1" stopColor="#0468D6" />
-        </linearGradient>
-
-        <linearGradient
-          id="gradient_after_animation1"
+          id="paint0_linear_5105_60482"
           x1="-14.3624"
-          y1="58.0019"
+          y1="58.0928"
           x2="-26.9979"
-          y2="17.7572"
+          y2="17.8482"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#EC008C" />
@@ -147,11 +75,11 @@ export function GovernanceIconHover(props: SVGProps<SVGSVGElement>) {
           <stop offset="1" stopColor="#0468D6" />
         </linearGradient>
         <linearGradient
-          id="gradient_after_animation2"
+          id="paint1_linear_5105_60482"
           x1="24.1714"
-          y1="-8.28598"
+          y1="-8.19504"
           x2="47.3894"
-          y2="-15.5757"
+          y2="-15.4848"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#EC008C" />
@@ -160,5 +88,124 @@ export function GovernanceIconHover(props: SVGProps<SVGSVGElement>) {
         </linearGradient>
       </defs>
     </svg>
+
+    // will comment out animated svg at the moment
+    // <svg
+    //   width="64"
+    //   height="51"
+    //   viewBox="0 0 64 51"
+    //   fill="none"
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   className={className}
+    // >
+    //   <path
+    //     d="M38.3332 0.0909424L63.3332 25.0909L38.3332 50.0909L13.3333 25.0909L38.3332 0.0909424Z"
+    //     fill="url(#paint0_linear_5602_70145)"
+    //   >
+    //     <animate
+    //       attributeName="d"
+    //       attributeType="XML"
+    //       from="M38.3332 0.0909424L63.3332 25.0909L38.3332 50.0909L13.3333 25.0909L38.3332 0.0909424Z"
+    //       to="M-8.60982e-07 30.303L19.697 10.6061L39.3939 30.303L19.697 50L-8.60982e-07 30.303Z"
+    //       begin="0s"
+    //       dur="0.3s"
+    //       fill="freeze"
+    //     />
+    //   </path>
+    //   <ellipse
+    //     cx="14.1664"
+    //     cy="25.9242"
+    //     rx="14.1667"
+    //     ry="14.1667"
+    //     fill="url(#paint1_linear_5602_70145)"
+    //   >
+    //     <animate
+    //       attributeName="cx"
+    //       attributeType="XML"
+    //       from="14.1664"
+    //       to="40.1515"
+    //       begin="0s"
+    //       dur="0.3s"
+    //       fill="freeze"
+    //     />
+    //     <animate
+    //       attributeName="cy"
+    //       attributeType="XML"
+    //       from="25.9242"
+    //       to="11.3636"
+    //       begin="0s"
+    //       dur="0.3s"
+    //       fill="freeze"
+    //     />
+    //     <animate
+    //       attributeName="r"
+    //       attributeType="XML"
+    //       from="14.1667"
+    //       to="11.3636"
+    //       begin="0s"
+    //       dur="0.3s"
+    //       fill="freeze"
+    //     />
+    //     {/* <animate
+    //       attributeName="fill"
+    //       from="url(#paint1_linear_5602_70145)"
+    //       to="gradient_after_animation2"
+    //       begin="0s"
+    //       dur="0.3s"
+    //       fill="freeze"
+    //     /> */}
+    //   </ellipse>
+    //   <defs>
+    //     <linearGradient
+    //       id="paint0_linear_5602_70145"
+    //       x1="3.17701"
+    //       y1="-18.1382"
+    //       x2="54.2567"
+    //       y2="-34.1757"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //     <linearGradient
+    //       id="paint1_linear_5602_70145"
+    //       x1="-5.75545"
+    //       y1="1.42771"
+    //       x2="23.1897"
+    //       y2="-7.66017"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+
+    //     <linearGradient
+    //       id="gradient_after_animation1"
+    //       x1="-14.3624"
+    //       y1="58.0019"
+    //       x2="-26.9979"
+    //       y2="17.7572"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //     <linearGradient
+    //       id="gradient_after_animation2"
+    //       x1="24.1714"
+    //       y1="-8.28598"
+    //       x2="47.3894"
+    //       y2="-15.5757"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //   </defs>
+    // </svg>
   );
 }
--- src/components/icons/assets/masternodesBenefits/StakingRewardsIcon.tsx
+++ src/components/icons/assets/masternodesBenefits/StakingRewardsIcon.tsx
@@ -66,45 +66,39 @@ export function StakingRewardsIconHover(props: SVGProps<SVGSVGElement>) {
       className={className}
     >
       <rect
-        width="50"
-        height="50"
-        transform="translate(0 0.0909424)"
-        fill="url(#paint0_linear_5601_70136)"
-      />
-      <rect
         x="49.5"
         y="0.590942"
         width="49"
         height="49"
         transform="rotate(90 49.5 0.590942)"
+        fill="url(#paint0_linear_6317_95)"
         stroke="black"
       />
       <rect
-        x="49.5"
-        y="0.590942"
-        width="40.6667"
-        height="40.6667"
-        transform="rotate(90 49.5 0.590942)"
-        fill="url(#paint1_linear_5601_70136)"
+        x="39.5"
+        y="10.5909"
+        width="30.6667"
+        height="30.6667"
+        transform="rotate(90 39.5 10.5909)"
+        fill="url(#paint1_linear_6317_95)"
         stroke="black"
       />
       <rect
-        id="staking-reward-icon-small-square"
-        x="49.5"
-        y="0.590942"
-        width="29"
-        height="29"
-        transform="rotate(90 49.5 0.590942)"
-        fill="url(#paint2_linear_5601_70136)"
-        stroke="red"
+        x="31.1665"
+        y="18.9243"
+        width="14"
+        height="14"
+        transform="rotate(90 31.1665 18.9243)"
+        fill="url(#paint2_linear_6317_95)"
+        stroke="black"
       />
       <defs>
         <linearGradient
-          id="paint0_linear_5601_70136"
-          x1="-10.1563"
-          y1="-18.2292"
-          x2="40.9235"
-          y2="-34.2666"
+          id="paint0_linear_6317_95"
+          x1="39.8438"
+          y1="-18.1382"
+          x2="90.9235"
+          y2="-34.1757"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#EC008C" />
@@ -112,11 +106,11 @@ export function StakingRewardsIconHover(props: SVGProps<SVGSVGElement>) {
           <stop offset="1" stopColor="#0468D6" />
         </linearGradient>
         <linearGradient
-          id="paint1_linear_5601_70136"
-          x1="41.5365"
-          y1="-15.1"
-          x2="84.1029"
-          y2="-28.4646"
+          id="paint1_linear_6317_95"
+          x1="33.5677"
+          y1="-1.4542"
+          x2="65.9182"
+          y2="-11.6112"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#EC008C" />
@@ -124,11 +118,11 @@ export function StakingRewardsIconHover(props: SVGProps<SVGSVGElement>) {
           <stop offset="1" stopColor="#0468D6" />
         </linearGradient>
         <linearGradient
-          id="paint2_linear_5601_70136"
-          x1="43.9063"
-          y1="-10.8466"
-          x2="74.5541"
-          y2="-20.469"
+          id="paint2_linear_6317_95"
+          x1="28.6196"
+          y1="12.9556"
+          x2="43.9436"
+          y2="8.14433"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#EC008C" />
@@ -137,5 +131,87 @@ export function StakingRewardsIconHover(props: SVGProps<SVGSVGElement>) {
         </linearGradient>
       </defs>
     </svg>
+
+    // will comment out animated svg at the moment
+    // <svg
+    //   width="50"
+    //   height="51"
+    //   viewBox="0 0 50 51"
+    //   fill="none"
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   className={className}
+    // >
+    //   <rect
+    //     width="50"
+    //     height="50"
+    //     transform="translate(0 0.0909424)"
+    //     fill="url(#paint0_linear_5601_70136)"
+    //   />
+    //   <rect
+    //     x="49.5"
+    //     y="0.590942"
+    //     width="49"
+    //     height="49"
+    //     transform="rotate(90 49.5 0.590942)"
+    //     stroke="black"
+    //   />
+    //   <rect
+    //     x="49.5"
+    //     y="0.590942"
+    //     width="40.6667"
+    //     height="40.6667"
+    //     transform="rotate(90 49.5 0.590942)"
+    //     fill="url(#paint1_linear_5601_70136)"
+    //     stroke="black"
+    //   />
+    //   <rect
+    //     id="staking-reward-icon-small-square"
+    //     x="49.5"
+    //     y="0.590942"
+    //     width="29"
+    //     height="29"
+    //     transform="rotate(90 49.5 0.590942)"
+    //     fill="url(#paint2_linear_5601_70136)"
+    //     stroke="red"
+    //   />
+    //   <defs>
+    //     <linearGradient
+    //       id="paint0_linear_5601_70136"
+    //       x1="-10.1563"
+    //       y1="-18.2292"
+    //       x2="40.9235"
+    //       y2="-34.2666"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //     <linearGradient
+    //       id="paint1_linear_5601_70136"
+    //       x1="41.5365"
+    //       y1="-15.1"
+    //       x2="84.1029"
+    //       y2="-28.4646"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //     <linearGradient
+    //       id="paint2_linear_5601_70136"
+    //       x1="43.9063"
+    //       y1="-10.8466"
+    //       x2="74.5541"
+    //       y2="-20.469"
+    //       gradientUnits="userSpaceOnUse"
+    //     >
+    //       <stop stopColor="#EC008C" />
+    //       <stop offset="0.846071" stopColor="#5B10FF" />
+    //       <stop offset="1" stopColor="#0468D6" />
+    //     </linearGradient>
+    //   </defs>
+    // </svg>
   );
 }

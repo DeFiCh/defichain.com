e8a7acd36ef3a7b4b4d568c8df35b52e8bb82dd8
chloe <44501120+chloezxyy@users.noreply.github.com>
2023-04-05 12:54:03 +0800

    fix: title whitespace

--- src/pages/explore/dfi/_components/ERC20Section.tsx
+++ src/pages/explore/dfi/_components/ERC20Section.tsx
@@ -7,21 +7,23 @@ export default function ERC20Section(): JSX.Element {
   const { t } = useTranslation("page-explore-dfi");
   const assetStyle =
     "bg-contain bg-top w-[333.31px] inset-0 bg-no-repeat left-1/2 transform -translate-x-1/2 -translate-y-[30%] top-[-30px] md:top-[-30px] lg:top-0";
+  const title = t("erc20Section.title").split(" ");
 
   return (
-    <Container className="relative pb-[96px] md:pb-[192px] lg:pb-[248px] text-center flex justify-center">
+    <Container className="relative pb-24 md:pb-48 lg:pb-[248px] text-center flex justify-center">
       <div className="max-w-[551px] flex flex-col items-center">
         <div
           className={classNames(
-            "text-[40px] leading-[44px] tracking-[-0.02em]",
+            "text-[40px] leading-[44px] tracking-[-0.02em] ",
             "lg:text-6xl lg:leading-[72px] lg:tracking-normal"
           )}
         >
-          {t("erc20Section.title")}
+          <span>{title[0]} </span>
+          <span className="whitespace-nowrap">{title[1]}</span>
         </div>
         <div
           className={classNames(
-            "pt-[20px] font-desc text-dark-800 text-base md:tracking-[0.03em]",
+            "pb-8 lg:pb-12 pt-5 font-desc text-dark-800 text-base md:tracking-[0.03em]",
             "lg:text-xl lg:tracking-normal"
           )}
         >
@@ -29,7 +31,7 @@ export default function ERC20Section(): JSX.Element {
         </div>
         <Button
           text={t("erc20Section.buttonText")}
-          className="mt-8 md:mt-[56px] lg:px-[72.5px] md:px-[130px] py-3 lg:py-4"
+          className="lg:px-[72.5px] md:px-[130px] py-3 lg:py-4"
           href="https://blog.defichain.com/defichains-first-move-to-become-an-interoperable-defi-powerhouse/"
         />
         <a
@@ -37,7 +39,7 @@ export default function ERC20Section(): JSX.Element {
           target="_blank"
           href="https://etherscan.io/token/0x8Fc8f8269ebca376D046Ce292dC7eaC40c8D358A"
           className={classNames(
-            "pt-4 font-desc text-dark-800 text-sm md:text-base font-semibold hover:accent-dfc-gradient-text cursor-pointer"
+            "pt-4 text-dark-800 text-sm md:text-base font-semibold hover:accent-dfc-gradient-text cursor-pointer"
           )}
         >
           <p>{t("erc20Section.contract")}</p>

9d5b6ed5a5712b3e2b8045af66a4795b4a0c0bcd
Chloe <44501120+chloezxyy@users.noreply.github.com>
2023-04-06 16:58:11 +0800

    feature(ui-ux): why dex section in explore page (#575)
    * feature(ui-ux): why dex section in explore page
    
    * fix: gap y between items
    
    * fix: desc text width
    
    * fix: text translation, link explore DEX button, added accent
    
    * enforce href prop in SecondaryButton component
    
    ---------
    
    Co-authored-by: Keng Ye <40191153+kyleleow@users.noreply.github.com>

---
+++ src/components/icons/assets/whyDex/AdvancedSwaps.tsx
@@ -0,0 +1,95 @@
+import { SVGProps } from "react";
+
+export function AdvancedSwapsIcon(props: SVGProps<SVGSVGElement>) {
+  const { className } = props;
+  return (
+    <svg
+      width="71"
+      height="47"
+      viewBox="0 0 71 47"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M38.3888 29.8589C35.624 32.6237 36.1766 35.9417 33.4119 33.177C30.6471 30.4122 29.8177 19.3543 32.5825 16.5896C35.3472 13.8248 33.1358 11.3354 35.9006 14.1001C38.6653 16.8649 41.1535 27.0941 38.3888 29.8589Z"
+        fill="url(#paint0_linear_2762_41385)"
+      />
+      <circle
+        cx="46.6829"
+        cy="23.4593"
+        r="16.0882"
+        transform="rotate(45 46.6829 23.4593)"
+        stroke="#D9D9D9"
+      />
+      <circle
+        cx="23.4593"
+        cy="23.4593"
+        r="16.0882"
+        transform="rotate(45 23.4593 23.4593)"
+        stroke="#D9D9D9"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_2762_41385"
+          x1="36.2251"
+          y1="6.56226"
+          x2="51.19"
+          y2="14.2025"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#EC008C" />
+          <stop offset="0.846071" stopColor="#5B10FF" />
+          <stop offset="1" stopColor="#0468D6" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export function AdvancedSwapsIconHover(props: SVGProps<SVGSVGElement>) {
+  const { className } = props;
+  return (
+    <svg
+      width="71"
+      height="47"
+      viewBox="0 0 71 47"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M38.3888 29.8589C35.624 32.6237 36.1766 35.9417 33.4119 33.177C30.6471 30.4122 29.8177 19.3543 32.5825 16.5896C35.3472 13.8248 33.1358 11.3354 35.9006 14.1001C38.6653 16.8649 41.1535 27.0941 38.3888 29.8589Z"
+        fill="url(#paint0_linear_2762_41385)"
+      />
+      <circle
+        cx="46.6829"
+        cy="23.4593"
+        r="16.0882"
+        transform="rotate(45 46.6829 23.4593)"
+        stroke="#D9D9D9"
+      />
+      <circle
+        cx="23.4593"
+        cy="23.4593"
+        r="16.0882"
+        transform="rotate(45 23.4593 23.4593)"
+        stroke="#D9D9D9"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_2762_41385"
+          x1="36.2251"
+          y1="6.56226"
+          x2="51.19"
+          y2="14.2025"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#EC008C" />
+          <stop offset="0.846071" stopColor="#5B10FF" />
+          <stop offset="1" stopColor="#0468D6" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}

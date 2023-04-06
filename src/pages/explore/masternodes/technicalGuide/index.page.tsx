495e45b3f3cf230f82aa91a243d14cc2c0cb164b
nattadex <elocinnat99@gmail.com>
2023-04-06 13:57:27 +0800

    edited the background asset for tablet

--- src/pages/explore/masternodes/technicalGuide/index.page.tsx
+++ src/pages/explore/masternodes/technicalGuide/index.page.tsx
@@ -21,19 +21,20 @@ export default function TechnicalGuide() {
   return (
     <>
       <div className="py-16 md:py-10 lg:py-16 border-b border-gray-800 border-opacity-50">
-        <Container className="flex flex-row justify-end relative">
-          <div className="block absolute w-[417px] h-[237px] top-[-158px] left-[-267px] md:top-[-42px] md:left-[-224px] lg:h-[297px] bg-contain bg-no-repeat mix-blend-screen bg-[url('/assets/img/mnTechnicalGuide/emptyCubeDesktop.png')]" />
-          <div className="flex flex-col">
+        <Container className="flex flex-row gap-x-12 relative lg:pr-12">
+          <div className="block absolute w-[417px] h-[237px] top-[-158px] left-[-267px] md:top-[-42px] md:left-[-281px] lg:h-[297px] bg-contain bg-no-repeat mix-blend-screen bg-[url('/assets/img/mnTechnicalGuide/emptyCubeDesktop.png')]" />
+          <div className="hidden lg:block md:w-3/12">&nbsp;</div>
+          <div className="flex flex-col w-full lg:w-9/12 md:px-6 lg:px-0">
             <div
               className={classNames(
-                "flex flex-col gap-y-5 md:w-[573px] lg:w-[812px]"
+                "flex flex-col gap-y-5 w-full lg:w-[812px]"
               )}
             >
               <SectionTitle text={t("technicalGuide.title")} />
               <SectionSubTitle text={t("technicalGuide.subtitle")} />
               <SectionDescription
                 text={t("technicalGuide.desc")}
-                customStyle="!text-base !tracking-[.03em] lg:tracking-normal lg:text-xl lg:leading-7"
+                customStyle="!text-base !tracking-[.03em] lg:!tracking-normal lg:!text-xl lg:!leading-7"
               />
             </div>
           </div>

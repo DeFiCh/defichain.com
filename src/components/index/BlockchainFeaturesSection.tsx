49fd176c95b39f6ec8313c229366142e301a063a
veralygit <57183851+veralygit@users.noreply.github.com>
2023-04-11 15:47:36 +0800

    update to use pt-20 instead of pt-[80px]

--- src/components/index/BlockchainFeaturesSection.tsx
+++ src/components/index/BlockchainFeaturesSection.tsx
@@ -43,7 +43,7 @@ export function BlockchainFeaturesSection(): JSX.Element {
 
   return (
     <section
-      className="lg:py-[156px] md:py-24 pt-[80px] pb-12"
+      className="lg:py-[156px] md:py-24 pt-20 pb-12"
       data-testid="BlockchainFeatureSection"
     >
       <Container className="flex flex-col justify-between lg:flex-row 2xl:max-w-[1920px] 2xl:mx-[300px]">

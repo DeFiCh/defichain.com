e44b5e1bff29d64c602a11769280676270163abc
Mikhail <mykhailo@cakedefi.com>
2023-04-11 20:57:23 +0800

    add blogs link checks;

---
+++ cypress/fixture/homepage.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/prefer-default-export */
+export const ecosystemLinks = [
+  {
+    title: "All about $DFI",
+    testid: "$DFI",
+    link: "/explore/dfi",
+  },
+  {
+    title: "Trade with DEX",
+    testid: "DEX",
+    link: "/explore/dex",
+  },
+  {
+    title: "Wallets for DeFiChain",
+    testid: "DeFiChain",
+    link: "explore/wallets",
+  },
+  {
+    title: "Governance: \nOwn a Masternode",
+    testid: "Masternode",
+    link: "explore/masternodes",
+  },
+];

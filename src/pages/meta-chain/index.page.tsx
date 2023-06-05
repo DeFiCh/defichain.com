1126704d8041789644b14963e63ef0b1083c326a
nattadex <elocinnat99@gmail.com>
2023-06-05 18:15:35 +0800

    updated the relative class

--- src/pages/meta-chain/index.page.tsx
+++ src/pages/meta-chain/index.page.tsx
@@ -11,29 +11,30 @@ export default function MetaChainPage() {
   const dimension = useWindowDimensions();
   return (
     <>
-      <Head title={t("heroBanner.subtitle")} />
-      <HeroBanner
-        title={t("heroBanner.title")}
-        subtitle={t("heroBanner.subtitle")}
-        subtitleCustomStyle="lg:w-[629px]"
-        desc={t("heroBanner.desc")}
-        heroBg={HeroBannerBg.META_CHAIN}
-        customBgPosStyle="lg:right-[-8em] md:right-[-5em] right-[-4em]"
-        hasStartExploringButton
-        // TODO: update start exploring link to accurate section
-        startExploringJumpLink="#metachain-key-features"
-        testID="meta-chain"
-      />
+      <div className="relative">
+        <Head title={t("heroBanner.subtitle")} />
+        <HeroBanner
+          title={t("heroBanner.title")}
+          subtitle={t("heroBanner.subtitle")}
+          subtitleCustomStyle="lg:w-[629px]"
+          desc={t("heroBanner.desc")}
+          heroBg={HeroBannerBg.META_CHAIN}
+          customBgPosStyle="lg:right-[-8em] md:right-[-5em] right-[-4em]"
+          hasStartExploringButton
+          // TODO: update start exploring link to accurate section
+          startExploringJumpLink="#metachain-key-features"
+          testID="meta-chain"
+        />
+        <div
+          className={classNames(
+            "jellyfish-item bg-[url(/assets/img/index/metachain-jellyfish.png)]",
+            "h-[300.93px] absolute inset-0 bg-contain bg-no-repeat md:bg-right",
+            "top-[10%] md:top-[16%] lg:top-[25%] left-[-35%] md:right-[35%] lg:right-[25%]",
+            { "right-[40%]": dimension.width <= 750 }
+          )}
+        />
+      </div>
       <MetaChainKeyFeatures />
-
-      <div
-        className={classNames(
-          "jellyfish-item bg-[url(/assets/img/index/metachain-jellyfish.png)]",
-          "h-[300.93px] absolute inset-0 bg-contain bg-no-repeat md:bg-right",
-          "top-[10%] md:top-[16%] lg:top-[25%] left-[-35%] md:right-[35%] lg:right-[25%]",
-          { "right-[40%]": dimension.width <= 750 }
-        )}
-      />
     </>
   );
 }

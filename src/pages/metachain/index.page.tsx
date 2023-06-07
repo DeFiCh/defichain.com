b5651853615b7c32d3224d8d8dfa8766f97e349b
nattadex <elocinnat99@gmail.com>
2023-06-07 14:11:15 +0800

    updated the background image

--- src/pages/metachain/index.page.tsx
+++ src/pages/metachain/index.page.tsx
@@ -9,33 +9,31 @@ export default function MetaChainPage() {
   const { t } = useTranslation("page-meta-chain");
   const dimension = useWindowDimensions();
   return (
-    <>
+    <div className="relative">
       <Head title={t("heroBanner.subtitle")} />
-      <div className="w-full">
-        <HeroBanner
-          customStyle="md:mb-[22px] lg:mb-[226px]"
-          title={t("heroBanner.title")}
-          subtitle={t("heroBanner.subtitle")}
-          subtitleCustomStyle="lg:w-[629px]"
-          desc={t("heroBanner.desc")}
-          heroBg={HeroBannerBg.META_CHAIN}
-          customBgPosStyle="lg:right-[-8em] md:right-[-5em] right-[-4em]"
-          hasStartExploringButton
-          // TODO: update start exploring link to accurate section
-          startExploringJumpLink="#statistics-display-dfi"
-          testID="meta-chain"
-        />
-      </div>
+      <HeroBanner
+        customStyle="md:mb-[22px] lg:mb-[226px]"
+        title={t("heroBanner.title")}
+        subtitle={t("heroBanner.subtitle")}
+        subtitleCustomStyle="lg:w-[629px]"
+        desc={t("heroBanner.desc")}
+        heroBg={HeroBannerBg.META_CHAIN}
+        customBgPosStyle="bg-auto lg:right-[-12em] lg:top-[-12em] lg:h-[1395px] md:right-[-15em] md:top-[-15em] md:h-[976px] h-auto top-[-22em] right-[-21em] left-[-2em]"
+        hasStartExploringButton
+        // TODO: update start exploring link to accurate section
+        startExploringJumpLink="#statistics-display-dfi"
+        testID="meta-chain"
+      />
 
       <div
         className={classNames(
           "jellyfish-item bg-[url(/assets/img/index/metachain-jellyfish.png)]",
           "h-[300.93px] absolute mix-blend-lighten inset-0 bg-contain bg-no-repeat md:bg-right",
-          "top-[10%] md:top-[16%] lg:top-[25%] left-[-35%] md:right-[35%] lg:right-[25%]",
+          "top-[10%] md:top-[16%] lg:top-[13%] left-[-35%] md:right-[35%] lg:right-[31%]",
           { "right-[40%]": dimension.width <= 750 }
         )}
       />
-    </>
+    </div>
   );
 }
 

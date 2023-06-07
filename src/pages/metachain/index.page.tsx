3d8dbf297809adbd87fd4d1f445e35c45d8f0d4e
nattadex <elocinnat99@gmail.com>
2023-06-07 14:00:08 +0800

    Revert "updated the background image"
    This reverts commit 9acc86c167a1bb1b97816b8652538c182a967707.

--- src/pages/metachain/index.page.tsx
+++ src/pages/metachain/index.page.tsx
@@ -20,7 +20,7 @@ export default function MetaChainPage() {
           subtitleCustomStyle="lg:w-[629px]"
           desc={t("heroBanner.desc")}
           heroBg={HeroBannerBg.META_CHAIN}
-          customBgPosStyle="bg-auto lg:right-[-12em] lg:top-[-12em] lg:h-[1395px] md:right-[-15em] md:top-[-15em] md:h-[976px] h-auto top-[-22em] right-[-21em] left-[-2em]"
+          customBgPosStyle="lg:right-[-8em] md:right-[-5em] right-[-4em]"
           hasStartExploringButton
           // TODO: update start exploring link to accurate section
           startExploringJumpLink="#metachain-key-features"
@@ -31,7 +31,7 @@ export default function MetaChainPage() {
           className={classNames(
             "jellyfish-item bg-[url(/assets/img/index/metachain-jellyfish.png)]",
             "h-[300.93px] absolute mix-blend-lighten inset-0 bg-contain bg-no-repeat md:bg-right",
-            "top-[10%] md:top-[16%] lg:top-[13%] left-[-35%] md:right-[35%] lg:right-[31%]",
+            "top-[10%] md:top-[16%] lg:top-[25%] left-[-35%] md:right-[35%] lg:right-[25%]",
             { "right-[40%]": dimension.width <= 750 }
           )}
         />

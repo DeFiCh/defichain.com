8390b6e388f0b835733be149df265ff5f2ac144e
nattadex <elocinnat99@gmail.com>
2023-06-09 19:20:37 +0800

    removed console log

--- src/pages/meta-chain/_components/MetaChainRoadmap.tsx
+++ src/pages/meta-chain/_components/MetaChainRoadmap.tsx
@@ -10,7 +10,6 @@ export function MetaChainRoadmap({ roadMapImg }): JSX.Element {
   const { t } = useTranslation("page-meta-chain");
   const dimension = useWindowDimensions();
   const mobileView = dimension.width <= 800;
-  console.log(roadMapImg);
   return (
     <section className={classNames("relative lg:mb-56 md:mb-52 mb-48")}>
       <Container>
@@ -51,7 +50,7 @@ export function MetaChainRoadmap({ roadMapImg }): JSX.Element {
           </div>
           {roadMapImg?.length > 0 ? (
             <div className="mt-12 lg:mt-0 lg:ml-auto max-w-[660px]">
-              {/* <Image alt="bleh" src={roadMapImg[0].metachain_roadmap.url} /> */}
+              {/* <Image alt="test" src={roadMapImg[-1].metachain_roadmap.url} /> */}
               <Image
                 width={660}
                 height={570}

59c92f395cc3465b6549e144b4b0132f73af20f9
nattadex <elocinnat99@gmail.com>
2023-06-09 18:53:28 +0800

    added e2e tests

--- cypress/e2e/metachain/metachain.spec.ts
+++ cypress/e2e/metachain/metachain.spec.ts
@@ -21,5 +21,63 @@ viewports.forEach((viewport) => {
         "Unrivaled innovation and interoperability, without compromising on scalability or security. MetaChain is for building dApps on the world's largest blockchains."
       );
     });
+
+    it("should have Start exploring button visible and navigate to the expected area", () => {
+      cy.findByTestId("start-exploring-button")
+        .scrollIntoView()
+        .then(() => {
+          if (viewport === "macbook-16") {
+            cy.findByTestId("start-exploring-button").click({
+              scrollBehavior: false,
+            });
+            cy.url().should("include", "#metachain-key-features");
+          }
+          cy.checkElementVisibilityAndText(
+            "section-title-metachain-key-features",
+            "KEY FEATURES"
+          );
+          cy.checkElementVisibilityAndText(
+            "title",
+            "Ethereum's power meets innovation"
+          );
+          cy.checkElementVisibilityAndText(
+            "desc",
+            "Provides an EVM-compatible environment that allows developers, familiar with the Ethereum ecosystem, to seamlessly and effortlessly build next generation applications on the Native DeFiChain."
+          );
+          // SVG icons
+          cy.checkElementVisibilityAndText(
+            "evm-&-utxo-integration-title",
+            "EVM & UTXO INTEGRATION"
+          );
+          cy.checkElementVisibilityAndText(
+            "evm-&-utxo-integration-desc",
+            "Supports both UTXO and Ethereum account model, providing privacy and simplicity seamlessly."
+          );
+          cy.checkElementVisibilityAndText(
+            "scalable-ecosystem-title",
+            "SCALABLE ECOSYSTEM"
+          );
+          cy.checkElementVisibilityAndText(
+            "scalable-ecosystem-desc",
+            "Unrestricted cross-compatibility empowers DFI users with extensive access to Ethereum and EVM-compatible chains."
+          );
+          cy.checkElementVisibilityAndText(
+            "low-transaction-fees-title",
+            "LOW TRANSACTION FEES"
+          );
+          cy.checkElementVisibilityAndText(
+            "low-transaction-fees-desc",
+            "Empowering builders and users with cost-effective transaction fees."
+          );
+          cy.checkElementVisibilityAndText(
+            "low-switching-costs-title",
+            "LOW SWITCHING COSTS"
+          );
+          cy.checkElementVisibilityAndText(
+            "low-switching-costs-desc",
+            "Seamless dApp deployment and effortless cross-chain interaction simplified using Meta Chain."
+          );
+        });
+    });
   });
 });

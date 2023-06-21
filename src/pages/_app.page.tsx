40d15080c396e5c107af30693adb29fa22fb2a53
nattadex <elocinnat99@gmail.com>
2023-06-21 14:34:59 +0800

    added meta pixel

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -30,6 +30,32 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         }}
         src="https://www.googletagmanager.com/gtag/js?id=G-HP6YZTCLBS"
       />
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel">
+        {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '222017136964976');
+        fbq('track', 'PageView');
+      `}
+      </Script>
+      <noscript>
+        <img
+          alt="facebook-pixel"
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=222017136964976&ev=PageView&noscript=1"
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
+
       <Component {...pageProps} />
     </Default>
   );

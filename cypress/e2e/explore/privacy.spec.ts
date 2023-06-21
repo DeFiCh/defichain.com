6a9c6a2319b8dcae18d4717ad14ab66a71ceabf0
Mikhail <mykhailo@cakedefi.com>
2023-06-21 16:53:23 +0800

    added test ids

--- cypress/e2e/explore/privacy.spec.ts
+++ cypress/e2e/explore/privacy.spec.ts
@@ -14,18 +14,18 @@ viewports.forEach((viewport) => {
         "PROTECTING YOUR PRIVACY"
       );
       cy.checkElementVisibilityAndText(
-        "section-header-explore-dex-decentralized-exchange",
+        "section-header-privacy-policy-protecting",
         "Privacy Policy"
       );
       cy.checkElementVisibilityAndText(
-        "section-desc-explore-dex-decentralized-exchange",
+        "section-desc-privacy-policy-protecting",
         "Discover our commitment to protecting your privacy through our strict privacy policy"
       );
     });
 
     it("should have all the expected Titles visible expected text", () => {
       titles.forEach((privacyTitle) => {
-        cy.get(`h2#${privacyTitle.id}`)
+        cy.findByTestId(privacyTitle.id)
           .scrollIntoView()
           .should("be.visible")
           .and("include.text", privacyTitle.title);
@@ -35,7 +35,7 @@ viewports.forEach((viewport) => {
     it("should be able to navigate on side menu", () => {
       if (viewport === "macbook-16") {
         titles.forEach((privacyTitle) => {
-          cy.get(`#${privacyTitle.id}`).click();
+          cy.findByTestId(privacyTitle.id).click();
           cy.checkElementVisibilityAndHref(
             `link-${privacyTitle.id}`,
             `#${privacyTitle.id}`

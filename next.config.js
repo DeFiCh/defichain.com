69965ecd30e8f9355f6a9ea9a03957f440107ee3
Ben Zumbrunn <benzumbrunn@gmail.com>
2025-02-06 18:33:24 +0800

    Fix netlify deployment (#747)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,86 @@
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value:
+      `default-src 'none';` +
+      `base-uri 'none';` +
+      `child-src 'self' app.netlify.com;` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self' images.prismic.io assets.coingecko.com s2.coinmarketcap.com *.cloudfront.net data:;` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com app.netlify.com netlify-cdp-loader.netlify.app *.googletagmanager.com ${
+        process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
+      } https://connect.facebook.net;` +
+      `script-src-elem 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com *.googletagmanager.com;` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline' files.coinmarketcap.com 'unsafe-inline';` +
+      `font-src 'self' fonts.gstatic.com;` +
+      `connect-src 'self' api.coingecko.com 3rdparty-apis.coinmarketcap.com wss://cable.coingecko.com ocean.defichain.com api.github.com *.google-analytics.com;`,
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
 module.exports = {
-  output: "export",
   experimental: {
     forceSwcTransforms: true,
   },
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.tsx", "page.ts"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
   images: {
     domains: ["images.prismic.io"],
     unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/learn/:path*",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/media",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/bug-bounty",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/security",
+        destination: "/404",
+        permanent: false,
+      },
+    ];
   },
 };

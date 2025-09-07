8dd28c59508bb267ae539ad35d7947cededb868f
defi2025 <theo@iancu.at>
2025-09-07 22:28:43 +0200

    Debug: revert CSP to next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 const securityHeaders = [
-  // Moved Content-Security-Policy to via netlify.toml
+  {
+    key: "Content-Security-Policy",
+    value:
+      `default-src 'none';` +
+      `base-uri 'none';` +
+      `child-src 'self' app.netlify.com;` +
+      `form-action 'none';` +
+      `frame-ancestors 'none';` +
+      `img-src 'self' images.prismic.io assets.coingecko.com s2.coinmarketcap.com *.cloudfront.net https://blogger.googleusercontent.com data:;` +
+      `media-src 'self';` +
+      `object-src 'none';` +
+      `script-src 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com app.netlify.com netlify-cdp-loader.netlify.app *.googletagmanager.com ${
+        process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
+      } https://connect.facebook.net;` +
+      `script-src-elem 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com *.googletagmanager.com;` +
+      `style-src 'self' fonts.googleapis.com 'unsafe-inline' files.coinmarketcap.com 'unsafe-inline';` +
+      `font-src 'self' fonts.gstatic.com;` +
+      `connect-src 'self' api.coingecko.com 3rdparty-apis.coinmarketcap.com wss://cable.coingecko.com ocean.defichain.com api.github.com *.google-analytics.com;`,
+  },
   {
     key: "Referrer-Policy",
     value: "same-origin",

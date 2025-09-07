6394e215c9e61974fad8d61dca76c43dc25bea2d
defi2025 <theo@iancu.at>
2025-09-07 23:12:08 +0200

    revert all changes

--- next.config.js
+++ next.config.js
@@ -7,21 +7,33 @@ const securityHeaders = [
       `child-src 'self' app.netlify.com;` +
       `form-action 'none';` +
       `frame-ancestors 'none';` +
-      `img-src 'self' images.prismic.io assets.coingecko.com s2.coinmarketcap.com *.cloudfront.net https://blogger.googleusercontent.com data:;` +
+      `img-src 'self' images.prismic.io assets.coingecko.com s2.coinmarketcap.com *.cloudfront.net data:;` +
       `media-src 'self';` +
       `object-src 'none';` +
       `script-src 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com app.netlify.com netlify-cdp-loader.netlify.app *.googletagmanager.com ${
         process.env.NODE_ENV === "development" ? `'unsafe-eval'` : ""
       } https://connect.facebook.net;` +
-      `script-src-elem 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com *.googletagmanager.com 'sha256-nECR5ZMSmALAY0Is6h2I1XmPYs9GXOZrQo37nfyAA0Y=';` +
+      `script-src-elem 'self' ajax.googleapis.com widgets.coingecko.com files.coinmarketcap.com 3rdparty-apis.coinmarketcap.com *.googletagmanager.com;` +
       `style-src 'self' fonts.googleapis.com 'unsafe-inline' files.coinmarketcap.com 'unsafe-inline';` +
       `font-src 'self' fonts.gstatic.com;` +
-      `connect-src 'self' api.coingecko.com 3rdparty-apis.coinmarketcap.com wss://cable.coingecko.com ocean.defichain.com api.github.com *.google-analytics.com region1.google-analytics.com;`,
+      `connect-src 'self' api.coingecko.com 3rdparty-apis.coinmarketcap.com wss://cable.coingecko.com ocean.defichain.com api.github.com *.google-analytics.com;`,
   },
-  { key: "Referrer-Policy", value: "same-origin" },
-  { key: "X-Content-Type-Options", value: "nosniff" },
-  { key: "X-Frame-Options", value: "DENY" },
-  { key: "X-XSS-Protection", value: "1; mode=block" },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
   {
     key: "Strict-Transport-Security",
     value: "max-age=63072000; includeSubDomains; preload",
@@ -29,23 +41,46 @@ const securityHeaders = [
 ];
 
 module.exports = {
-  experimental: { forceSwcTransforms: true },
+  experimental: {
+    forceSwcTransforms: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.tsx", "page.ts"],
   async headers() {
-    return [{ source: "/(.*)", headers: securityHeaders }];
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
   images: {
-    domains: ["images.prismic.io", "blogger.googleusercontent.com"],
+    domains: ["images.prismic.io"],
     unoptimized: true,
   },
   async redirects() {
     return [
-      { source: "/learn/:path*", destination: "/404", permanent: false },
-      { source: "/media", destination: "/404", permanent: false },
-      { source: "/bug-bounty", destination: "/404", permanent: false },
-      { source: "/security", destination: "/404", permanent: false },
+      {
+        source: "/learn/:path*",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/media",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/bug-bounty",
+        destination: "/404",
+        permanent: false,
+      },
+      {
+        source: "/security",
+        destination: "/404",
+        permanent: false,
+      },
     ];
   },
 };
